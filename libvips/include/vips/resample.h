e951668e45b646fc42ec6d1c112e7a23560dbb0f
John Cupitt <jcupitt@gmail.com>
2012-11-07 14:54:50 +0000

    started adding im_transform()

--- libvips/include/vips/resample.h
+++ libvips/include/vips/resample.h
@@ -44,6 +44,8 @@ int im_affinei( VipsImage *in, VipsImage *out,
 int im_affinei_all( VipsImage *in, VipsImage *out, VipsInterpolate *interpolate,
 	double a, double b, double c, double d, double dx, double dy ) ;
 
+int im_transform2( VipsImage *in, VipsImage *out, DOUBLEMASK *vec );
+
 int im_rightshift_size( VipsImage *in, VipsImage *out, 
 	int xshift, int yshift, int band_fmt );
 
