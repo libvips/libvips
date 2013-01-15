9599b5dc9297f3ceba0fd645825c1498a2bfa203
John Cupitt <jcupitt@gmail.com>
2013-01-15 22:14:24 +0000

    remove RCS tag
    thanks testn

--- libvips/iofuncs/base64.c
+++ libvips/iofuncs/base64.c
@@ -17,8 +17,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  * 
- * $Id: base64.c,v 1.5 2007/01/03 18:31:34 jcupitt Exp $
- *
  * \section{Base-64 Routines}
  *
  * \intro These routines use the 64-character subset of International
