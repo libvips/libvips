8cc76a6ba35eb8aa0f9216aa243c671de2ebc517
Benjamin Gilbert <bgilbert@backtick.net>
2013-03-07 00:40:19 -0500

    Update address for Free Software Foundation
    Also update LGPLv2.1 license text from the copy currently on the GNU
    website.

--- libvips/resample/lbb.cpp
+++ libvips/resample/lbb.cpp
@@ -22,9 +22,9 @@
     Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public
-    License along with this program; if not, write to the Free
-    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-    02111-1307 USA
+    License along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA
 
  */
 
