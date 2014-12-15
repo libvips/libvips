71bb0e81d1095666619fb04e3879ec24ef726570
John Cupitt <jcupitt@gmail.com>
2014-12-15 18:12:47 +0000

    better Vips.py install
    more work on test_convolution

--- test/test_convolution.py
+++ test/test_convolution.py
@@ -6,6 +6,7 @@ from builtins import range
 from numbers import Number
 
 import unittest
+import operator
 import math
 
 #import logging
@@ -56,10 +57,20 @@ def conv(image, mask, x_position, y_position):
         for y in range(0, mask.height):
             m = mask.getpoint(x, y)
             i = image.getpoint(x + x_position, y + y_position)
-            p = run_fn2(lambda a, b: a * b, m, i)
-            s = run_fn2(lambda a, b: a + b, s, p)
+            p = run_fn2(operator.mul, m, i)
+            s = run_fn2(operator.add, s, p)
 
-    return run_fn2(lambda a, b: a / b, s, mask.get_scale())
+    return run_fn2(operator.div, s, mask.get_scale())
+
+def compass(image, mask, x_position, y_position, n_rot, fn):
+    acc = []
+    for i in range(0, n_rot):
+        result = conv(image, mask, x_position, y_position)
+        result = run_fn(abs, result)
+        acc.append(result)
+        mask = mask.rot45()
+
+    return reduce(lambda a, b: run_fn2(fn, a, b), acc)
 
 class TestConvolution(unittest.TestCase):
     # test a pair of things which can be lists for approx. equality
@@ -73,15 +84,19 @@ class TestConvolution(unittest.TestCase):
         self.colour = im * [1, 2, 3] + [2, 3, 4]
         self.mono = self.colour.extract_band(1)
         self.all_images = [self.mono, self.colour]
-        sharp = Vips.Image.new_from_array([[-1, -1,  -1], 
-                                           [-1,  16, -1], 
-                                           [-1, -1,  -1]], scale = 8)
-        blur = Vips.Image.new_from_array([[1, 1, 1], 
-                                          [1, 1, 1], 
-                                          [1, 1, 1]], scale = 9)
-        line = Vips.Image.new_from_array([[1, 1, 1], [-2, -2, -2], [1, 1, 1]])
-        sobel = Vips.Image.new_from_array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
-        self.all_masks = [sharp, blur, line, sobel]
+        self.sharp = Vips.Image.new_from_array([[-1, -1,  -1], 
+                                                [-1,  16, -1], 
+                                                [-1, -1,  -1]], scale = 8)
+        self.blur = Vips.Image.new_from_array([[1, 1, 1], 
+                                               [1, 1, 1], 
+                                               [1, 1, 1]], scale = 9)
+        self.line = Vips.Image.new_from_array([[ 1,  1,  1], 
+                                               [-2, -2, -2], 
+                                               [ 1,  1,  1]])
+        self.sobel = Vips.Image.new_from_array([[ 1,  2,  1], 
+                                                [ 0,  0,  0], 
+                                                [-1, -2, -1]])
+        self.all_masks = [self.sharp, self.blur, self.line, self.sobel]
 
     def test_conv(self):
         for im in self.all_images:
@@ -97,7 +112,29 @@ class TestConvolution(unittest.TestCase):
                     true = conv(im, msk, 49, 49)
                     self.assertAlmostEqualObjects(result, true)
 
+    def test_x(self):
+        im = self.mono
+        msk = self.sobel
 
+        result = conv(im, msk, 24, 49)
+        msk = msk.rot45()
+
+        result = conv(im, msk, 24, 49)
+        msk = msk.rot45()
+
+    def test_compass(self):
+        for im in self.all_images:
+            for msk in self.all_masks:
+                for prec in [Vips.Precision.INTEGER, Vips.Precision.FLOAT]:
+                    convolved = im.compass(msk, 
+                                           times = 3, 
+                                           angle = Vips.Angle45.D45,
+                                           combine = Vips.Combine.MAX,
+                                           precision = prec)
+
+                    result = convolved.getpoint(25, 50)
+                    true = compass(im, msk, 24, 49, 3, max)
+                    self.assertAlmostEqualObjects(result, true)
 
 if __name__ == '__main__':
     unittest.main()
