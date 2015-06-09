3591623a12c85dcf8824539ffd25ec62b6fcf281
JonDeen <jonas@ogaard.no>
2015-06-09 17:43:08 +0200

    casting hell

--- libvips/colour/sRGB2HSV.c
+++ libvips/colour/sRGB2HSV.c
@@ -100,7 +100,7 @@ vips_sRGB2HSV_line( VipsColour *colour, VipsPel *out, VipsPel **in, int width )
 				q[0] = (unsigned char) ((secondary_diff / (float) delta) + wrap_around_hue);
 			}
 
-			q[1] = (((float) delta / (float) c_max)*256.0f);
+			q[1] = (((float) delta / (float) c_max)*256);
 		}
 
 		p += 3;
