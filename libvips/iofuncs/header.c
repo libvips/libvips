e4c53e59dd18538c6a3d324ad3039a1f5756f5e4
John Cupitt <jcupitt@gmail.com>
2015-06-20 10:54:24 +0100

    fix a compile error

--- libvips/iofuncs/header.c
+++ libvips/iofuncs/header.c
@@ -482,7 +482,6 @@ vips_image_guess_interpretation( const VipsImage *image )
 	case VIPS_INTERPRETATION_sRGB: 
 	case VIPS_INTERPRETATION_HSV: 
 	case VIPS_INTERPRETATION_scRGB: 
-	case VIPS_INTERPRETATION_HSV:
 	case VIPS_INTERPRETATION_YXY: 
 		if( image->Bands < 3 )
 			sane = FALSE;
