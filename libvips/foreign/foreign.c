128480bab6ec648e2c0b45b8bc1f99e9b0aabc19
John Cupitt <jcupitt@gmail.com>
2016-04-23 13:29:42 +0100

    VipsForeign nickname was wrong
    it was set to "file", strangely
    
    see https://github.com/jcupitt/libvips/issues/429

--- libvips/foreign/foreign.c
+++ libvips/foreign/foreign.c
@@ -357,7 +357,7 @@ vips_foreign_class_init( VipsForeignClass *class )
 	gobject_class->set_property = vips_object_set_property;
 	gobject_class->get_property = vips_object_get_property;
 
-	object_class->nickname = "file";
+	object_class->nickname = "foreign";
 	object_class->description = _( "load and save image files" );
 	object_class->summary_class = vips_foreign_summary_class;
 }
