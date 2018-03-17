ad13dd204189f50a4bfe1559bbb5dd29ade37913
John Cupitt <jcupitt@gmail.com>
2018-03-17 18:10:50 +0000

    add a high-precision path for canny

--- libvips/convolution/canny.c
+++ libvips/convolution/canny.c
@@ -1,5 +1,4 @@
 /* Canny edge detector
- * 
  */
 
 /*
@@ -55,7 +54,6 @@ typedef struct _VipsCanny {
 	VipsImage *out;
 
 	double sigma; 
-	gboolean interpolate;
 
 	/* Need an image vector for start_many.
 	 */
@@ -66,35 +64,33 @@ typedef VipsOperationClass VipsCannyClass;
 
 G_DEFINE_TYPE( VipsCanny, vips_canny, VIPS_TYPE_OPERATION );
 
-/* Simple 2x2 -1/+1 difference. 
+/* Simple 2x2 -1/+1 difference. For uchar, we try to hit the vector path and
+ * use an offset rather than -ves. Otherwise it's float or double output.
  */
 static int
-vips_canny_gradient_simple( VipsImage *in, VipsImage **Gx, VipsImage **Gy )
+vips_canny_gradient( VipsImage *in, VipsImage **Gx, VipsImage **Gy )
 {
 	VipsImage *scope;
 	VipsImage **t;
+	VipsPrecision precision;
 
 	scope = vips_image_new();
-	t = (VipsImage **) vips_object_local_array( (VipsObject *) scope, 20 );
+	t = (VipsImage **) vips_object_local_array( (VipsObject *) scope, 2 );
 
-	t[1] = vips_image_new_matrixv( 2, 2, 
+	t[0] = vips_image_new_matrixv( 2, 2, 
 		-1.0, 1.0,
 		-1.0, 1.0 );
-	vips_image_set_double( t[1], "offset", 128.0 ); 
-	if( vips_conv( in, Gx, t[1], 
-		"precision", VIPS_PRECISION_INTEGER,
-		NULL ) ) {
-		g_object_unref( scope ); 
-		return( -1 );
-	}
 
-	t[5] = vips_image_new_matrixv( 2, 2, 
-		-1.0, -1.0,
-		 1.0,  1.0 );
-	vips_image_set_double( t[5], "offset", 128.0 ); 
-	if( vips_conv( in, Gy, t[5], 
-		"precision", VIPS_PRECISION_INTEGER,
-		NULL ) ) { 
+	if( in->BandFmt == VIPS_FORMAT_UCHAR ) {
+		precision = VIPS_PRECISION_INTEGER;
+		vips_image_set_double( t[0], "offset", 128.0 ); 
+	}
+	else 
+		precision = VIPS_PRECISION_FLOAT;
+
+	if( vips_conv( in, Gx, t[0], "precision", precision, NULL ) ||
+		vips_rot90( t[0], &t[1], NULL ) ||
+		vips_conv( in, Gy, t[1], "precision", precision, NULL ) ) { 
 		g_object_unref( scope ); 
 		return( -1 );
 	}
@@ -107,6 +103,58 @@ vips_canny_gradient_simple( VipsImage *in, VipsImage **Gx, VipsImage **Gy )
 /* LUT for calculating atan2() with +/- 4 bits of precision in each axis.
  */
 static VipsPel vips_canny_polar_atan2[256];
+
+/* For the uchar path, gx/gy are -128 to +127, and we need -8 to +7 for the 
+ * atan2 LUT. Try "256 * (VIPS_DEG( atan2( gx, gy ) ) + 360) / 360;" to test  
+ * the LUT. 
+ *
+ * For G, we should calculate sqrt( gx * gx + gy * gy ), however we are only
+ * interested in relative magnitude (max of sqrt), so we can skip the sqrt
+ * itself. We need a result that will fit in 0 - 255, so shift down.
+ */
+#define POLAR_UCHAR { \
+	for( x = 0; x < r->width; x++ ) { \
+		for( band = 0; band < Gx->Bands; band++ ) { \
+			int gx = p1[band] - 128; \
+			int gy = p2[band] - 128; \
+			\
+			int i = ((gx >> 4) & 0xf) | (gy & 0xf0); \
+			\
+			q[0] = (gx * gx + gy * gy + 256) >> 9; \
+			q[1] = vips_canny_polar_atan2[i]; \
+			\
+			q += 2; \
+		} \
+		\
+		p1 += Gx->Bands; \
+		p2 += Gx->Bands; \
+	} \
+}
+
+/* Float/double path. We keep the same ranges as the uchar path to reduce
+ * confusion. 
+ */
+#define POLAR( TYPE ) { \
+	TYPE *tp1 = (TYPE *) p1; \
+	TYPE *tp2 = (TYPE *) p2; \
+	TYPE *tq = (TYPE *) q; \
+	\
+	for( x = 0; x < r->width; x++ ) { \
+		for( band = 0; band < Gx->Bands; band++ ) { \
+			double gx = tp1[band]; \
+			double gy = tp2[band]; \
+			double theta = VIPS_DEG( atan2( gx, gy ) ); \
+			\
+			tq[0] = (gx * gx + gy * gy) / 512.0; \
+			tq[1] = 256.0 * fmod( theta + 360.0, 360.0 ) / 360.0; \
+			\
+			tq += 2; \
+		} \
+		\
+		tp1 += Gx->Bands; \
+		tp2 += Gx->Bands; \
+	} \
+}
 
 static int
 vips_canny_polar_generate( VipsRegion *or, 
@@ -129,34 +177,21 @@ vips_canny_polar_generate( VipsRegion *or,
 		VipsPel *q = (VipsPel * restrict) 
 			VIPS_REGION_ADDR( or, r->left, r->top + y );
 
-		for( x = 0; x < r->width; x++ ) {
-			for( band = 0; band < Gx->Bands; band++ ) { 
-				int gx = p1[band] - 128;
-				int gy = p2[band] - 128;
+		switch( Gx->BandFmt ) {
+		case VIPS_FORMAT_UCHAR:
+			POLAR_UCHAR;
+			break;
 
-				/* gx/gy are -128 to +127, we need -8 to +7
-				 * for the atan2 LUT.
-				 */
-				int i = ((gx >> 4) & 0xf) | (gy & 0xf0);
+		case VIPS_FORMAT_FLOAT:
+			POLAR( float );
+			break;
 
-				/* We should calculate 
-				 * 	0.5 * sqrt( x * x + y * y )
-				 * ie. length of hypot, scaled down to avoid
-				 * clipping. We are only interested in relative
-				 * magnitude, so we can skip the sqrt and just
-				 * shift down 9 bits.
-				 *
-				 * Try "256 * (VIPS_DEG( atan2( gx, gy ) ) + 
-				 * 360) / 360;" to test LUT.
-				 */
-				q[0] = (gx * gx + gy * gy + 256) >> 9;
-				q[1] = vips_canny_polar_atan2[i];
+		case VIPS_FORMAT_DOUBLE:
+			POLAR( double );
+			break;
 
-				q += 2;
-			}
-
-			p1 += Gx->Bands;
-			p2 += Gx->Bands;
+		default:
+			g_assert( FALSE );
 		}
 	}
 
@@ -183,12 +218,11 @@ vips_atan2_init( void *null )
 	return( NULL ); 
 }
 
-/* Calculate G/theta from Gx/Gy -- rather like rect -> polar, except that we
- * code theta as below. Scale G down by 0.5 so that we don't clip on hard 
- * edges. 
+/* Calculate G/theta from Gx/Gy. We code theta as 0-256 for 0-360
+ * and skip the sqrt on G.
  *
- * For a white disc on a black background, theta is 0 at the bottom, 64 on the
- * right, 128 at the top and 192 on the left edge. 
+ * For a white disc on a black background, theta is 0 at the top, 64 on the
+ * left, 128 on the right and 192 on the right edge. 
  */
 static int
 vips_canny_polar( VipsImage **args, VipsImage **out )
@@ -211,109 +245,73 @@ vips_canny_polar( VipsImage **args, VipsImage **out )
 	return( 0 );
 }
 
-static int
-vips_canny_nonmax_generate( VipsRegion *or, 
-	void *vseq, void *a, void *b, gboolean *stop )
-{
-	VipsRegion *in = (VipsRegion *) vseq;
-	VipsRect *r = &or->valid;
-	VipsImage *im = in->im;
-	int out_bands = or->im->Bands;
-
-	VipsRect rect;
-	int x, y, band; 
-	int lsk;
-	int psk;
-	int offseta[4];
-	int offsetb[4];
-
-	rect = *r;
-	rect.width += 2;
-	rect.height += 2;
-	if( vips_region_prepare( in, &rect ) )
-		return( -1 );
-	lsk = VIPS_REGION_LSKIP( in ); 
-	psk = VIPS_IMAGE_SIZEOF_PEL( im ); 
-
-	/* For each of the four directions, the offset to get to that pixel
-	 * from the top-left of our 3x3. offseta is the left/up direction, or
-	 * the lower memory address.
-	 *
-	 *   1 | 0 | 3
-	 *   --+---+--
-	 *   2 | X | 2
-	 *   --+---+--
-	 *   3 | 0 | 1
-	 */
-	offseta[0] = psk;
-	offsetb[0] = psk + 2 * lsk;
-	offseta[1] = 0;
-	offsetb[1] = 2 * psk + 2 * lsk;
-	offseta[2] = lsk;
-	offsetb[2] = 2 * psk + lsk;
-	offseta[3] = 2 * psk;
-	offsetb[3] = 2 * lsk;
-
-	for( y = 0; y < r->height; y++ ) {
-		VipsPel *p = (VipsPel * restrict) 
-			VIPS_REGION_ADDR( in, r->left, r->top + y );
-		VipsPel *q = (VipsPel * restrict) 
-			VIPS_REGION_ADDR( or, r->left, r->top + y );
-
-		for( x = 0; x < r->width; x++ ) {
-			for( band = 0; band < out_bands; band++ ) { 
-				int G = p[lsk + psk];
-				int theta = p[lsk + psk + 1];
-				int a = ((theta + 16) / 32) & 0x3;
-				VipsPel low = p[offseta[a]];
-				VipsPel high = p[offsetb[a]];
-
-				/* Set G to 0 if it's not the local maxima in
-				 * the direction of the gradient. If G is equal
-				 * to the low side, also zero, so wide edges
-				 * with equal gradient move (arbitarilly) left 
-				 * and up.
-				 */
-				if( G <= low ||
-					G < high )
-					G = 0;
-
-				q[band] = G;
-
-				p += 2;
-			}
-
-			q += out_bands;
-		}
-	}
-
-	return( 0 );
-}
-
-/* Remove non-maximal edges. At each point, compare the G to the G in either
- * direction and 0 it if it's not the largest.
+/* Set G to 0 if it's not the local maxima in the direction of the gradient. 
  */
-static int
-vips_canny_nonmax( VipsImage *in, VipsImage **out )
-{
-	*out = vips_image_new();
-	if( vips_image_pipelinev( *out, 
-		VIPS_DEMAND_STYLE_THINSTRIP, in, NULL ) )
-		return( -1 );
-	(*out)->Bands /= 2;
-	(*out)->Xsize -= 2;
-	(*out)->Ysize -= 2;
+#define THIN_UCHAR { \
+	for( x = 0; x < r->width; x++ ) { \
+		for( band = 0; band < out_bands; band++ ) {  \
+			int G = p[lsk + psk]; \
+			int theta = p[lsk + psk + 1]; \
+			int low_theta = (theta / 32) & 0x7; \
+			int high_theta = (low_theta + 1) & 0x7; \
+			int residual = theta - low_theta * 32; \
+			int lowa = p[offset[low_theta]]; \
+			int lowb = p[offset[high_theta]]; \
+			int low = (lowa * (32 - residual) +  \
+				lowb * residual) / 32; \
+			int higha = p[offset[(low_theta + 4) & 0x7]]; \
+			int highb = p[offset[(high_theta + 4) & 0x7]]; \
+			int high = (higha * (32 - residual) + \
+				highb * residual) / 32; \
+			\
+			if( G <= low || \
+				G < high ) \
+				G = 0; \
+			\
+			q[band] = G; \
+			\
+			p += 2; \
+		} \
+		\
+		q += out_bands; \
+	} \
+}
 
-	if( vips_image_generate( *out, 
-		vips_start_one, vips_canny_nonmax_generate, vips_stop_one, 
-		in, NULL ) )
-		return( -1 );
-
-	return( 0 );
+#define THIN( TYPE ) { \
+	TYPE *tp = (TYPE *) p; \
+	TYPE *tq = (TYPE *) q; \
+	\
+	for( x = 0; x < r->width; x++ ) { \
+		for( band = 0; band < out_bands; band++ ) {  \
+			TYPE G = tp[lsk + psk]; \
+			TYPE theta = tp[lsk + psk + 1]; \
+			int low_theta = ((int) (theta / 32)) & 0x7; \
+			int high_theta = (low_theta + 1) & 0x7; \
+			TYPE residual = theta - low_theta * 32; \
+			TYPE lowa = tp[offset[low_theta]]; \
+			TYPE lowb = tp[offset[high_theta]]; \
+			TYPE low = (lowa * (32 - residual) + \
+				lowb * residual) / 32; \
+			TYPE higha = tp[offset[(low_theta + 4) & 0x7]]; \
+			TYPE highb = tp[offset[(high_theta + 4) & 0x7]]; \
+			TYPE high = (higha * (32 - residual) + \
+				highb * residual) / 32; \
+			\
+			if( G <= low || \
+				G < high ) \
+				G = 0; \
+			\
+			tq[band] = G; \
+			\
+			tp += 2; \
+		} \
+		\
+		tq += out_bands; \
+	} \
 }
 
 static int
-vips_canny_nonmaxi_generate( VipsRegion *or, 
+vips_canny_thin_generate( VipsRegion *or, 
 	void *vseq, void *a, void *b, gboolean *stop )
 {
 	VipsRegion *in = (VipsRegion *) vseq;
@@ -333,26 +331,29 @@ vips_canny_nonmaxi_generate( VipsRegion *or,
 	rect.height += 2;
 	if( vips_region_prepare( in, &rect ) )
 		return( -1 );
-	lsk = VIPS_REGION_LSKIP( in ); 
-	psk = VIPS_IMAGE_SIZEOF_PEL( im ); 
+
+	/* These are in typed units.
+	 */
+	lsk = VIPS_REGION_LSKIP( in ) / VIPS_IMAGE_SIZEOF_ELEMENT( im ); 
+	psk = VIPS_IMAGE_SIZEOF_PEL( im ) / VIPS_IMAGE_SIZEOF_ELEMENT( im );
 
 	/* For each of the 8 directions, the offset to get to that pixel from
 	 * the top-left of the 3x3.
 	 *
-	 *   5 | 4 | 3
+	 *   1 | 0 | 7
 	 *   --+---+--
-	 *   6 | X | 2
+	 *   2 | X | 6
 	 *   --+---+--
-	 *   7 | 0 | 1
+	 *   3 | 4 | 5
 	 */
-	offset[0] = psk + 2 * lsk;
-	offset[1] = 2 * psk + 2 * lsk;
-	offset[2] = 2 * psk + lsk;
-	offset[3] = 2 * psk;
-	offset[4] = psk;
-	offset[5] = 0;
-	offset[6] = lsk;
-	offset[7] = 2 * lsk;
+	offset[0] = psk;
+	offset[1] = 0;
+	offset[2] = lsk;
+	offset[3] = 2 * lsk;
+	offset[4] = 2 * lsk + psk;
+	offset[5] = 2 * lsk + 2 * psk;
+	offset[6] = lsk + 2 * psk;
+	offset[7] = 2 * psk;
 
 	for( y = 0; y < r->height; y++ ) {
 		VipsPel *p = (VipsPel * restrict) 
@@ -360,70 +361,22 @@ vips_canny_nonmaxi_generate( VipsRegion *or,
 		VipsPel *q = (VipsPel * restrict) 
 			VIPS_REGION_ADDR( or, r->left, r->top + y );
 
-		for( x = 0; x < r->width; x++ ) {
-			for( band = 0; band < out_bands; band++ ) { 
-				int G = p[lsk + psk];
-				int theta = p[lsk + psk + 1];
-				int low_theta = (theta / 32) & 0x7;
-				int high_theta = (low_theta + 1) & 0x7;
-				int residual = theta - low_theta * 32;
-				int lowa = p[offset[low_theta]];
-				int lowb = p[offset[high_theta]];
-				int low = (lowa * (32 - residual) + 
-						lowb * residual) / 32;
-				int higha = p[offset[(low_theta + 4) & 0x7]];
-				int highb = p[offset[(high_theta + 4) & 0x7]];
-				int high = (higha * (32 - residual) + 
-						highb * residual) / 32;
+		switch( im->BandFmt ) {
+		case VIPS_FORMAT_UCHAR:
+			//THIN_UCHAR;
+			THIN( unsigned char );
+			break;
 
-#ifdef DEBUG
-if( G > 0 ) { 
-	int i, j;
+		case VIPS_FORMAT_FLOAT:
+			THIN( float );
+			break;
 
-	printf( "Gradient:\n" ); 
-	for( j = 0; j < 3; j++ ) {
-		for( i = 0; i < 3; i++ ) 
-			printf( "%3d ", p[j * lsk + i * psk] );
-		printf("\n");
-	}
+		case VIPS_FORMAT_DOUBLE:
+			THIN( double );
+			break;
 
-	printf( "Theta:\n" ); 
-	for( j = 0; j < 3; j++ ) {
-		for( i = 0; i < 3; i++ ) 
-			printf( "%3d ", p[j * lsk + i * psk + 1] );
-		printf("\n");
-	}
-
-	printf( "G = %d\n", G ); 
-	printf( "theta = %d\n", theta ); 
-	printf( "low_theta = %d\n", low_theta ); 
-	printf( "high_theta = %d\n", high_theta ); 
-	printf( "residual = %d\n", residual ); 
-	printf( "lowa = %d\n", lowa ); 
-	printf( "lowb = %d\n", lowb ); 
-	printf( "low = %d\n", low ); 
-	printf( "higha = %d\n", higha ); 
-	printf( "highb = %d\n", highb ); 
-	printf( "high = %d\n", high ); 
-	if( G <= low || G < high )
-		printf( "thin\n" ); 
-	printf( "\n" ); 
-}
-#endif /*DEBUG*/
-
-				/* Set G to 0 if it's not the local maxima in
-				 * the direction of the gradient. 
-				 */
-				if( G <= low ||
-					G < high )
-					G = 0;
-
-				q[band] = G;
-
-				p += 2;
-			}
-
-			q += out_bands;
+		default:
+			g_assert( FALSE );
 		}
 	}
 
@@ -434,7 +387,7 @@ if( G > 0 ) {
  * direction and 0 it if it's not the largest.
  */
 static int
-vips_canny_nonmaxi( VipsImage *in, VipsImage **out )
+vips_canny_thin( VipsImage *in, VipsImage **out )
 {
 	*out = vips_image_new();
 	if( vips_image_pipelinev( *out, 
@@ -445,7 +398,7 @@ vips_canny_nonmaxi( VipsImage *in, VipsImage **out )
 	(*out)->Ysize -= 2;
 
 	if( vips_image_generate( *out, 
-		vips_start_one, vips_canny_nonmaxi_generate, vips_stop_one, 
+		vips_start_one, vips_canny_thin_generate, vips_stop_one, 
 		in, NULL ) )
 		return( -1 );
 
@@ -467,20 +420,14 @@ vips_canny_build( VipsObject *object )
 
 	in = canny->in;
 
-	/* We are 8-bit only.
-	 */
-	if( vips_cast_uchar( in, &t[4], NULL ) )
-		return( -1 );
-	in = t[4];
-
 	if( vips_gaussblur( in, &t[0], canny->sigma, NULL ) )
 		return( -1 );
 	in = t[0];
 
-	if( vips_canny_gradient_simple( in, &Gx, &Gy ) )
+	if( vips_canny_gradient( in, &Gx, &Gy ) )
 		return( -1 ); 
 
-	/* Form (G, theta), with theta coded.
+	/* Form (G, theta).
 	 */
 	canny->args[0] = Gx;
 	canny->args[1] = Gy;
@@ -489,20 +436,16 @@ vips_canny_build( VipsObject *object )
 		return( -1 ); 
 	in = t[1];
 
-	/* Expand by two pixels all around, then thin.
+	/* Expand by two pixels all around, then thin in the direction of the
+	 * gradient.
 	 */
 	if( vips_embed( in, &t[2], 1, 1, in->Xsize + 2, in->Ysize + 2,
 		"extend", VIPS_EXTEND_COPY,
 		NULL ) )
 		return( -1 );
-	if( canny->interpolate ) {
-		if( vips_canny_nonmaxi( t[2], &t[3] ) )
-			return( -1 );
-	}
-	else {
-		if( vips_canny_nonmax( t[2], &t[3] ) )
-			return( -1 );
-	}
+
+	if( vips_canny_thin( t[2], &t[3] ) )
+		return( -1 );
 	in = t[3];
 
 	g_object_set( object, "out", vips_image_new(), NULL ); 
@@ -524,7 +467,7 @@ vips_canny_class_init( VipsCannyClass *class )
 	gobject_class->get_property = vips_object_get_property;
 
 	object_class->nickname = "canny";
-	object_class->description = _( "gaussian blur" );
+	object_class->description = _( "Canny edge detector" );
 	object_class->build = vips_canny_build;
 
 	operation_class->flags = VIPS_OPERATION_SEQUENTIAL;
@@ -548,13 +491,6 @@ vips_canny_class_init( VipsCannyClass *class )
 		G_STRUCT_OFFSET( VipsCanny, sigma ),
 		0.01, 1000, 1.4 );
 
-	VIPS_ARG_BOOL( class, "interpolate", 13, 
-		_( "Interpolate" ), 
-		_( "Interpolate gradient angles" ),
-		VIPS_ARGUMENT_OPTIONAL_INPUT,
-		G_STRUCT_OFFSET( VipsCanny, interpolate ),
-		FALSE );
-
 }
 
 static void
@@ -573,7 +509,6 @@ vips_canny_init( VipsCanny *canny )
  * Optional arguments:
  *
  * * @sigma: %gdouble, sigma for gaussian blur
- * * @interpolate: %gboolean, interpolate gradient on corners 
  *
  * Find edges by Canny's method: The maximum of the derivative of the gradient
  * in the direction of the gradient. This operation will only work well on
@@ -581,9 +516,6 @@ vips_canny_init( VipsCanny *canny )
  *
  * Use @sigma to control the scale over which gradient is measured. 1.4 is
  * usually a good value.
- *
- * Set @interpolate to smoothly interpolate the gradient around corners. This
- * slows down edge detection, but can improve the result.
  *
  * You will probably need to process the output further to eliminate weak 
  * edges. 
