libvips/libvips

commit eefb2dad980c12922ad15d987be7c4b42a44d5cf

improve rounding in convi intize
We were rounding up with ceil() when intize-ing convolution masks.
However, the vector path has a true range of (1.0, -1.0], so a mask with
1.0 as the max (for example) was actually triggering the overflow detector
and falling back to the C path.

Round up with ceil(x + 1) instead, so 1.0 (for example) will be mapped
to 0.5 and won't overflow.

libvips/convolution/convi.c
@@ -904,8 +904,12 @@ vips_convi_intize( VipsConvi *convi, VipsImage *M )
 
 	/* The mask max rounded up to the next power of two gives the exponent
 	 * all elements share. Values are eg. -3 for 1/8, 3 for 8.
+	 *
+	 * Add one so we round up stuff exactly on x.0. We multiply by 128
+	 * later, so 1.0 (for example) would become 128, which is outside
+	 * signed 8 bit. 
 	 */
-	shift = ceil( log2( mx ) );
+	shift = ceil( log2( mx + 1 ) );
 
 	/* We need to sum n_points, so we have to shift right before adding a
 	 * new value to make sure we have enough range. 

commit 2041deb917eab85271ee2fbfcb97ecba662ad05a

Fix convolution vector path posterization
This fixes posterization in the vector path for convolution caused by a
typo in commit eefb2dad980c12922ad15d987be7c4b42a44d5cf.

The fix was suggested by @jcupitt.

libvips/convolution/convi.c
@@ -909,7 +909,7 @@ vips_convi_intize( VipsConvi *convi, VipsImage *M )
 	 * later, so 1.0 (for example) would become 128, which is outside
 	 * signed 8 bit. 
 	 */
-	shift = ceil( log2( mx + 1 ) );
+	shift = ceil( log2( mx ) + 1 );
 
 	/* We need to sum n_points, so we have to shift right before adding a
 	 * new value to make sure we have enough range. 