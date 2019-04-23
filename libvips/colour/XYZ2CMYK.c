libvips/libvips

commit 56d98f3ef06aa20b94d6b23e15365595fcabee57

thumbnail no longer needs to import cmyk
since colourspace knows about cmyk now

libvips/resample/thumbnail.c
@@ -18,6 +18,8 @@
  * 	- implement shrink-on-load for tiff pyramid 
  * 3/2/19 kleisauke
  * 	- add option_string param to thumbnail_buffer
+ * 23/4/19
+ * 	- don't force import CMYK, since colourspace knows about it now
  */
 
 /*
@@ -568,16 +570,12 @@ vips_thumbnail_build( VipsObject *object )
 
 	/* In linear mode, we import right at the start. 
 	 *
-	 * We also have to import the whole image if it's CMYK, since
-	 * vips_colourspace() (see below) doesn't know about CMYK.
-	 *
 	 * This is only going to work for images in device space. If you have
 	 * an image in PCS which also has an attached profile, strange things
 	 * will happen. 
 	 */
 	have_imported = FALSE;
-	if( (thumbnail->linear ||
-		in->Type == VIPS_INTERPRETATION_CMYK) &&
+	if( thumbnail->linear &&
 		in->Coding == VIPS_CODING_NONE &&
 		(in->BandFmt == VIPS_FORMAT_UCHAR ||
 		 in->BandFmt == VIPS_FORMAT_USHORT) &&

libvips/iofuncs/init.c
@@ -291,13 +291,14 @@ set_stacksize( guint64 size )
 		return;
 	}
 
-	if( cur_stack_size < size )
+	if( cur_stack_size < size ) {
 		if( pthread_attr_setstacksize( &attr, size ) ||
 			pthread_setattr_default_np( &attr ) ) 
 			g_warning( "set_stacksize: unable to set stack size" );
 		else 
 			g_info( "set stack size to %" G_GUINT64_FORMAT "k", 
 				size / (guint64) 1024 );
+	}
 #endif /*HAVE_PTHREAD_DEFAULT_NP*/
 }
 

commit d2a9a20a78497041d0e1fa8ae4da3bde8ae587a5

fix thumbnail regression with cmyk in and out
56d98f3ef06aa20b94d6b23e15365595fcabee57 broke cmyk->cymk thumbnails

libvips/colour/XYZ2CMYK.c
@@ -3,6 +3,8 @@
  *
  * 21/12/18
  *      - from CMYK2XYZ.c
+ * 09/01/2019
+ *  	- add CMYK <-> XYZ conversions if no lcms2 has been found
  */
 
 /*

libvips/colour/colourspace.c
@@ -21,8 +21,6 @@
  * 	  https://github.com/lovell/sharp/issues/193
  * 27/12/18
  * 	- add CMYK conversions
- * 09/01/2019
- *  - add CMYK <-> XYZ conversions if no lcms2 has been found
  */
 
 /*

libvips/resample/thumbnail.c
@@ -599,8 +599,14 @@ vips_thumbnail_build( VipsObject *object )
 		have_imported = TRUE;
 	}
 
-	/* To the processing colourspace. This will unpack LABQ as well.
+	/* To the processing colourspace. This will unpack LABQ, import CMYK,
+	 * etc.
+	 *
+	 * If this is a CMYK image, we need to set have_imported since we only
+	 * want to export at the end.
 	 */
+	if( in->Type == VIPS_INTERPRETATION_CMYK )
+		have_imported = TRUE;
 	g_info( "converting to processing space %s",
 		vips_enum_nick( VIPS_TYPE_INTERPRETATION, interpretation ) ); 
 	if( vips_colourspace( in, &t[2], interpretation, NULL ) ) 