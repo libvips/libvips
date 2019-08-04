503e83e23211764670c1ed32424d7f053e825a91
John Cupitt <jcupitt@gmail.com>
2019-08-04 17:43:35 +0100

    update resize C++ example

--- cplusplus/examples/resize.cpp
+++ cplusplus/examples/resize.cpp
@@ -1,11 +1,7 @@
-/* 
- * compile with:
+/* compile with:
  *
  *      g++ -g -Wall resize.cpp `pkg-config vips-cpp --cflags --libs`
- *
  */
-
-#define DEBUG
 
 #include <vips/vips8>
 
@@ -14,16 +10,15 @@ using namespace vips;
 int
 main( int argc, char **argv )
 {
-	if( vips_init( argv[0] ) )
+	if( VIPS_INIT( argv[0] ) )
 		vips_error_exit( NULL ); 
 
-	VImage in = VImage::new_from_file( argv[1], 
-		VImage::option()->set( "access", VIPS_ACCESS_SEQUENTIAL ) ); 
-	VInterpolate interp = VInterpolate::new_from_name( "nohalo" );
+	VImage in = VImage::new_from_file( argv[1], VImage::option()
+		->set( "access", "sequential" ) ); 
 
-	VImage out; 
-
-	out = in.resize( 0.2, VImage::option()->set( "interpolate", interp ) );
+	VImage out = in.resize( 0.2, VImage::option()
+		->set( "kernel", "cubic" )
+		->set( "vscale", 0.2 ) );
 
 	out.write_to_file( argv[2] );
 
