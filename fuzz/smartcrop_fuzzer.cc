16a5cac2e352cfd661ee56f113103d451afd8b64
Oscar Mira <oscar.mira@schibsted.com>
2019-08-19 15:59:55 +0200

    add fuzzers for vips_smartcrop and vip_mosaic

---
+++ fuzz/smartcrop_fuzzer.cc
@@ -0,0 +1,39 @@
+#include <vips/vips.h>
+
+extern "C" int
+LLVMFuzzerInitialize( int *argc, char ***argv )
+{
+	vips_concurrency_set( 1 );
+	return( 0 );
+}
+
+extern "C" int
+LLVMFuzzerTestOneInput( const guint8 *data, size_t size )
+{
+	VipsImage *image, *out;
+	double d;
+
+	if( !(image = vips_image_new_from_buffer( data, size, "", NULL )) )
+		return( 0 );
+
+	/* Skip big images. They are likely to timeout.
+	 */
+	if( image->Xsize > 1024 ||
+	    image->Ysize > 1024 ||
+	    image->Bands > 10 ) {
+		g_object_unref( image );
+		return( 0 );
+	}
+
+	if( vips_smartcrop( image, &out, 32, 32, NULL ) ) {
+		g_object_unref( image );
+		return( 0 );
+	}
+
+	vips_min( out, &d, NULL );
+
+	g_object_unref( out );
+	g_object_unref( image );
+
+	return( 0 );
+}
