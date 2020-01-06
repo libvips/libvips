9609f4f1d784016cd86f11b7fda9ad23bf6a9bda
Andrii Dmytrenko <andrii@fetlife.com>
2020-01-06 11:30:27 +0000

    Check if orientation is less than 1

--- libvips/foreign/exif.c
+++ libvips/foreign/exif.c
@@ -525,9 +525,8 @@ vips__exif_parse( VipsImage *image )
 		int orientation;
 
 		orientation = atoi( str );
-		if ( orientation < 0 || orientation > 8 ) {
+		if( orientation < 1 || orientation > 8 )
 			orientation = 1;
-		}
 		vips_image_set_int( image, VIPS_META_ORIENTATION, orientation );
 	}
 
