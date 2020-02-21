fe47ce12e9d016f6705f2f5aad5209a9f719b34d
John Cupitt <jcupitt@gmail.com>
2020-02-21 17:57:28 +0000

    move csvsave to the new target API

--- libvips/foreign/foreign.c
+++ libvips/foreign/foreign.c
@@ -2036,7 +2036,8 @@ vips_foreign_operation_init( void )
 
 	extern GType vips_foreign_load_csv_file_get_type( void ); 
 	extern GType vips_foreign_load_csv_source_get_type( void ); 
-	extern GType vips_foreign_save_csv_get_type( void ); 
+	extern GType vips_foreign_save_csv_file_get_type( void ); 
+	extern GType vips_foreign_save_csv_target_get_type( void ); 
 
 	extern GType vips_foreign_load_matrix_get_type( void ); 
 	extern GType vips_foreign_save_matrix_get_type( void ); 
@@ -2112,7 +2113,8 @@ vips_foreign_operation_init( void )
 
 	vips_foreign_load_csv_file_get_type(); 
 	vips_foreign_load_csv_source_get_type(); 
-	vips_foreign_save_csv_get_type(); 
+	vips_foreign_save_csv_file_get_type(); 
+	vips_foreign_save_csv_target_get_type(); 
 	vips_foreign_load_matrix_get_type(); 
 	vips_foreign_save_matrix_get_type(); 
 	vips_foreign_print_matrix_get_type(); 
