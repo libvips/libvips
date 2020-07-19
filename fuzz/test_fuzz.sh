5953f9eba9adc4db6795a7f340e798245423e053
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-07-19 19:09:09 +0200

    Ensure UBSan exits with a non-zero code on error

--- fuzz/test_fuzz.sh
+++ fuzz/test_fuzz.sh
@@ -7,8 +7,8 @@ set -e
 
 # Glib is built without -fno-omit-frame-pointer. We need
 # to disable the fast unwinder to get full stacktraces.
-export ASAN_OPTIONS="fast_unwind_on_malloc=0:allocator_may_return_null=1"
-export UBSAN_OPTIONS="print_stacktrace=1"
+export ASAN_OPTIONS="$ASAN_OPTIONS:fast_unwind_on_malloc=0:allocator_may_return_null=1"
+export UBSAN_OPTIONS="$UBSAN_OPTIONS:print_stacktrace=1"
 
 # Hide all warning messages from vips.
 export VIPS_WARNING=0
@@ -17,7 +17,9 @@ ret=0
 
 for fuzzer in *_fuzzer; do
   for file in $top_srcdir/fuzz/common_fuzzer_corpus/*; do
-    if ! ./$fuzzer $file; then
+    exit_code=0
+    ./$fuzzer $file || exit_code=$?
+    if [ $exit_code -ne 0 ]; then
       echo FAIL $fuzzer $file
       ret=1
     fi
