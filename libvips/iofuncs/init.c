a63d37fc9b46247f4da2d04ae8657b5ee0420597
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-02-13 14:15:27 +0100

    Initial support for dynamic loadable modules with GModule

--- libvips/iofuncs/init.c
+++ libvips/iofuncs/init.c
@@ -510,13 +510,18 @@ vips_init( const char *argv0 )
 	vips_mosaicing_operation_init();
 	vips_g_input_stream_get_type(); 
 
-	/* Load any vips8 plugins from the vips libdir. Keep going, even if
-	 * some plugins fail to load. 
+	/* Load any vips8 modules from the vips libdir. Keep going, even if
+	 * some modules fail to load. 
+	 */
+	(void) vips_load_plugins( "%s/vips-modules-%d.%d", 
+		libdir, VIPS_MAJOR_VERSION, VIPS_MINOR_VERSION );
+
+#if ENABLE_DEPRECATED
+	/* Load any vips8 plugins from the vips libdir.
 	 */
 	(void) vips_load_plugins( "%s/vips-plugins-%d.%d", 
 		libdir, VIPS_MAJOR_VERSION, VIPS_MINOR_VERSION );
 
-#if ENABLE_DEPRECATED
 	/* Load up any vips7 plugins in the vips libdir. We don't error on 
 	 * failure, it's too annoying to have VIPS refuse to start because of 
 	 * a broken plugin.
