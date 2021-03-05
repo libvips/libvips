b14bac53a33b1461978df0f5deb4a46c727ddc79
John Cupitt <jcupitt@gmail.com>
2021-03-05 12:16:38 +0000

    guard refs to ZSTD
    so we work on older libtiffs

--- libvips/foreign/vips2tiff.c
+++ libvips/foreign/vips2tiff.c
@@ -659,12 +659,15 @@ wtiff_write_header( Wtiff *wtiff, Layer *layer )
 		TIFFSetField( tif, TIFFTAG_WEBP_LEVEL, wtiff->Q );
 		TIFFSetField( tif, TIFFTAG_WEBP_LOSSLESS, wtiff->lossless );
 	}
-	if( wtiff->compression == COMPRESSION_ZSTD ) 
+	if( wtiff->compression == COMPRESSION_ZSTD ) {
 		TIFFSetField( tif, TIFFTAG_ZSTD_LEVEL, wtiff->level );
+		if( wtiff->predictor != VIPS_FOREIGN_TIFF_PREDICTOR_NONE ) 
+			TIFFSetField( tif, 
+				TIFFTAG_PREDICTOR, wtiff->predictor );
+	}
 #endif /*HAVE_TIFF_COMPRESSION_WEBP*/
 
 	if( (wtiff->compression == COMPRESSION_ADOBE_DEFLATE ||
-		wtiff->compression == COMPRESSION_ZSTD ||
 		wtiff->compression == COMPRESSION_LZW) &&
 		wtiff->predictor != VIPS_FOREIGN_TIFF_PREDICTOR_NONE ) 
 		TIFFSetField( tif, TIFFTAG_PREDICTOR, wtiff->predictor );
@@ -1863,12 +1866,15 @@ wtiff_copy_tiff( Wtiff *wtiff, TIFF *out, TIFF *in )
 		TIFFSetField( out, TIFFTAG_WEBP_LEVEL, wtiff->Q );
 		TIFFSetField( out, TIFFTAG_WEBP_LOSSLESS, wtiff->lossless );
 	}
-	if( wtiff->compression == COMPRESSION_ZSTD ) 
+	if( wtiff->compression == COMPRESSION_ZSTD ) {
 		TIFFSetField( out, TIFFTAG_ZSTD_LEVEL, wtiff->level );
+		if( wtiff->predictor != VIPS_FOREIGN_TIFF_PREDICTOR_NONE ) 
+			TIFFSetField( out, 
+				TIFFTAG_PREDICTOR, wtiff->predictor );
+	}
 #endif /*HAVE_TIFF_COMPRESSION_WEBP*/
 
 	if( (wtiff->compression == COMPRESSION_ADOBE_DEFLATE ||
-		wtiff->compression == COMPRESSION_ZSTD ||
 		wtiff->compression == COMPRESSION_LZW) &&
 		wtiff->predictor != VIPS_FOREIGN_TIFF_PREDICTOR_NONE ) 
 		TIFFSetField( out, TIFFTAG_PREDICTOR, wtiff->predictor );
