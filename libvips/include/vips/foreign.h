fb6891d02b4744ec8e3057c68ce7e55ced47f822
John Cupitt <jcupitt@gmail.com>
2021-03-20 14:22:06 +0000

    typo in header

--- libvips/include/vips/foreign.h
+++ libvips/include/vips/foreign.h
@@ -684,7 +684,7 @@ int vips_jp2kload_source( VipsSource *source, VipsImage **out, ... )
 	__attribute__((sentinel));
 int vips_jp2ksave( VipsImage *in, const char *filename, ... )
 	__attribute__((sentinel));
-int vips_heifsave_buffer( VipsImage *in, void **buf, size_t *len, ... )
+int vips_jp2ksave_buffer( VipsImage *in, void **buf, size_t *len, ... )
 	__attribute__((sentinel));
 int vips_jp2ksave_target( VipsImage *in, VipsTarget *target, ... )
 	__attribute__((sentinel));
