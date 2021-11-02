libvips/libvips

commit f92069b0353f3aebbe01793bf49a66895506b444

Incorporate #2506 also in {webp,magick7}load (#2507)

libvips/foreign/magick7load.c
@@ -601,7 +601,7 @@ vips_foreign_load_magick7_parse( VipsForeignLoadMagick7 *magick7,
 
 	/* So we can finally set the height.
 	 */
-	if( vips_object_argument_isset( VIPS_OBJECT( magick7 ), "n" ) ) {
+	if( read->n_frames > 1 ) {
 		vips_image_set_int( out, VIPS_META_PAGE_HEIGHT, out->Ysize );
 		out->Ysize *= magick7->n_frames;
 	}

libvips/foreign/webp2vips.c
@@ -519,8 +519,12 @@ read_header( Read *read, VipsImage *out )
 #endif /*DEBUG*/
 
 	if( flags & ANIMATION_FLAG ) { 
-		vips_image_set_int( out, 
-			VIPS_META_PAGE_HEIGHT, read->frame_height );
+		/* Only set page-height if we have more than one page, or
+		 * this could accidentally turn into an animated image later.
+		 */
+		if( read->n > 1 )
+			vips_image_set_int( out, 
+				VIPS_META_PAGE_HEIGHT, read->frame_height );
 
 		read->width = read->frame_width;
 		read->height = read->n * read->frame_height;

commit 629454c7fbd35265e0fbeda4d481bf658912e5cc

Fix typo in `magick7load`
Introduced in commit f92069b.

libvips/foreign/magick7load.c
@@ -601,7 +601,7 @@ vips_foreign_load_magick7_parse( VipsForeignLoadMagick7 *magick7,
 
 	/* So we can finally set the height.
 	 */
-	if( read->n_frames > 1 ) {
+	if( magick7->n_frames > 1 ) {
 		vips_image_set_int( out, VIPS_META_PAGE_HEIGHT, out->Ysize );
 		out->Ysize *= magick7->n_frames;
 	}