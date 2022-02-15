69bf2be12b0e1023275cde963006495df2d68377
Corentin Noël <corentin.noel@collabora.com>
2022-02-15 11:54:40 +0100

    Enhance the introspection support by fixing annotations (#2657)
    Also refine the .metadata file

--- libvips/include/vips/transform.h
+++ libvips/include/vips/transform.h
@@ -31,6 +31,10 @@
 #ifndef VIPS_TRANSFORM_H
 #define VIPS_TRANSFORM_H
 
+#include <glib.h>
+#include <vips/image.h>
+#include <vips/rect.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif /*__cplusplus*/
