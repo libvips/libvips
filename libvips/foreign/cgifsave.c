e8b85fbfbb81c8db186ee3dd5f7592a4a9bbd1fa
Essem <smswessem@gmail.com>
2022-03-04 10:34:19 -0600

    cgifsave: Change loop count logic

--- libvips/foreign/cgifsave.c
+++ libvips/foreign/cgifsave.c
@@ -63,7 +63,6 @@ typedef struct _VipsForeignSaveCgif {
 	int effort;
 	int bitdepth;
 	double maxerror;
-	gboolean noloop;
 	VipsTarget *target;
 
 	/* Derived write params.
@@ -311,11 +310,11 @@ vips_foreign_save_cgif_write_frame( VipsForeignSaveCgif *cgif )
 	 */
 	if( !cgif->cgif_context ) {
 		cgif->cgif_config.pGlobalPalette = cgif->palette_rgb;
-		cgif->cgif_config.attrFlags = CGIF_ATTR_IS_ANIMATED | ( cgif->noloop ? CGIF_ATTR_NO_LOOP : 0 );
+		cgif->cgif_config.attrFlags = CGIF_ATTR_IS_ANIMATED | ( cgif->loop == 1 ? CGIF_ATTR_NO_LOOP : 0 );
 		cgif->cgif_config.width = frame_rect->width;
 		cgif->cgif_config.height = frame_rect->height;
 		cgif->cgif_config.numGlobalPaletteEntries = cgif->lp->count;
-		cgif->cgif_config.numLoops = cgif->loop;
+		cgif->cgif_config.numLoops = cgif->loop > 1 ? cgif->loop - 1 : cgif->loop;
 		cgif->cgif_config.pWriteFn = vips__cgif_write;
 		cgif->cgif_config.pContext = (void *) cgif->target;
 
@@ -614,13 +613,6 @@ vips_foreign_save_cgif_class_init( VipsForeignSaveCgifClass *class )
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsForeignSaveCgif, maxerror ),
 		0, 32, 0.0 );
-
-	VIPS_ARG_BOOL( class, "noloop", 14,
-		_( "No loop" ),
-		_( "Disables looping" ),
-		VIPS_ARGUMENT_OPTIONAL_INPUT,
-		G_STRUCT_OFFSET( VipsForeignSaveCgif, noloop ),
-		FALSE );
 }
 
 static void
