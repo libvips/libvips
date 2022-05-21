7d4cba101c294b3d9cc98bef36624d7e7f446cfd
Lovell Fuller <lovell@users.noreply.github.com>
2022-05-21 09:49:31 +0100

    jxl: remove support for never-implemented JXL_TYPE_UINT32 (#2813)

--- libvips/foreign/jxlload.c
+++ libvips/foreign/jxlload.c
@@ -350,10 +350,6 @@ vips_foreign_load_jxl_print_format( JxlPixelFormat *format )
 		printf( "JXL_TYPE_UINT16" );
 		break;
 
-	case JXL_TYPE_UINT32: 
-		printf( "JXL_TYPE_UINT32" );
-		break;
-
 	case JXL_TYPE_FLOAT: 
 		printf( "JXL_TYPE_FLOAT" );
 		break;
@@ -420,10 +416,6 @@ vips_foreign_load_jxl_set_header( VipsForeignLoadJxl *jxl, VipsImage *out )
 
 	case JXL_TYPE_UINT16:
 		format = VIPS_FORMAT_USHORT;
-		break;
-
-	case JXL_TYPE_UINT32:
-		format = VIPS_FORMAT_UINT;
 		break;
 
 	case JXL_TYPE_FLOAT:
@@ -551,8 +543,6 @@ vips_foreign_load_jxl_header( VipsForeignLoad *load )
 			if( jxl->info.exponent_bits_per_sample > 0 ||
 				jxl->info.alpha_exponent_bits > 0 )
 				jxl->format.data_type = JXL_TYPE_FLOAT;
-			else if( jxl->info.bits_per_sample > 16 )
-				jxl->format.data_type = JXL_TYPE_UINT32;
 			else if( jxl->info.bits_per_sample > 8 )
 				jxl->format.data_type = JXL_TYPE_UINT16;
 			else
