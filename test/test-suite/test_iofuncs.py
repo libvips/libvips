673a593161c58f8277258067fff3249d661928dc
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-07-28 13:37:35 +0200

    test: remove `TestIofuncs::test_split7` (#2954)
    See: https://github.com/libvips/pyvips/pull/341

--- test/test-suite/test_iofuncs.py
+++ test/test-suite/test_iofuncs.py
@@ -2,42 +2,9 @@
 import pytest
 
 import pyvips
-from helpers import assert_equal_objects
 
 
 class TestIofuncs:
-    # test the vips7 filename splitter ... this is very fragile and annoying
-    # code with lots of cases
-
-    @pytest.mark.xfail(raises=AttributeError, reason="uses deprecated symbols")
-    def test_split7(self):
-        def split(path):
-            filename7 = pyvips.path_filename7(path)
-            mode7 = pyvips.path_mode7(path)
-
-            return [filename7, mode7]
-
-        cases = [
-            ["c:\\silly:dir:name\\fr:ed.tif:jpeg:95,,,,c:\\icc\\srgb.icc",
-             ["c:\\silly:dir:name\\fr:ed.tif",
-              "jpeg:95,,,,c:\\icc\\srgb.icc"]],
-            ["I180:",
-             ["I180",
-              ""]],
-            ["c:\\silly:",
-             ["c:\\silly",
-              ""]],
-            ["c:\\program files\\x:hello",
-             ["c:\\program files\\x",
-              "hello"]],
-            ["C:\\fixtures\\2569067123_aca715a2ee_o.jpg",
-             ["C:\\fixtures\\2569067123_aca715a2ee_o.jpg",
-              ""]]
-        ]
-
-        for case in cases:
-            assert_equal_objects(split(case[0]), case[1])
-
     def test_new_from_image(self):
         im = pyvips.Image.mask_ideal(100, 100, 0.5,
                                      reject=True, optical=True)
