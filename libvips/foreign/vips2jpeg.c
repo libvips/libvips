2c4c0390564da4acf4186663b6bcd1c4ccbe96d8
John Cupitt <jcupitt@gmail.com>
2022-08-02 13:50:09 +0100

    add "unlimited" to jpegload
    To disable DoS limits for JPEG loading. Adding API on a stable branch is
    bad, but this fixes a regression, so I think it's necessary,
    unfortunately.
    
    See https://github.com/libvips/libvips/issues/2973

--- libvips/foreign/vips2jpeg.c
+++ libvips/foreign/vips2jpeg.c
@@ -173,12 +173,6 @@ vips__new_output_message( j_common_ptr cinfo )
 #ifdef DEBUG
 	printf( "vips__new_output_message: \"%s\"\n", buffer );
 #endif /*DEBUG*/
-
-	/* This is run for things like file truncated. Signal invalidate to
-	 * force this op out of cache. 
-	 */
-        if( cinfo->client_data )
-		vips_foreign_load_invalidate( VIPS_IMAGE( cinfo->client_data ) );
 }
 
 /* New error_exit handler.
