0f56b099d0b2e001dc5a6f686cb09d2b0e44fc34
John Cupitt <jcupitt@gmail.com>
2022-08-05 09:14:28 +0100

    remove dbg

--- libvips/foreign/svgload.c
+++ libvips/foreign/svgload.c
@@ -573,11 +573,11 @@ vips_foreign_load_svg_generate( VipsRegion *or,
 	int y;
 
 #ifdef DEBUG
-#endif /*DEBUG*/
 	printf( "vips_foreign_load_svg_generate: %p \n     "
 		"left = %d, top = %d, width = %d, height = %d\n", 
 		svg,
 		r->left, r->top, r->width, r->height ); 
+#endif /*DEBUG*/
 
 	/* rsvg won't always paint the background.
 	 */
