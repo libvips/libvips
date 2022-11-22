be5feaefcfe29079e1df5c186ea52dc6f46d7930
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-11-22 17:30:48 +0100

    Adapt profiles.c to .clang-format style

--- libvips/colour/wrap-profiles.sh
+++ libvips/colour/wrap-profiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# code up the binary files in $1 as a set of name / string pairs 
-# in $2
+# code up the binary files in $1 as a set of name / string pairs in $2
+# For example:
+# $ ./wrap-profiles.sh profiles profiles.c
 
 # we have to use arrays for the strings, since MSVC won't allow string
 # literals larger than 64kb
@@ -9,7 +10,8 @@
 in=$1
 out=$2
 
-echo "/* this file generated automatically, do not edit */" > $out
+echo "/* this file is generated automatically, do not edit! */" > $out
+echo "/* clang-format off */" > $out
 echo "" >> $out
 echo "#include \"profiles.h\"" >> $out
 echo "" >> $out
@@ -21,18 +23,18 @@ for file in $in/*.icm; do
   profile_name=vips__profile_fallback_$base
   profile_names="$profile_names $profile_name"
   echo "static VipsProfileFallback $profile_name = {" >> $out
-  echo "    \"$base\"," >> $out
-  echo "    $(stat --format=%s $file)," >> $out
-  echo "    {" >> $out
+  echo -e "\t\"$base\"," >> $out
+  echo -e "\t$(stat --format=%s $file)," >> $out
+  echo -e "\t{" >> $out
   pigz -c -z -11 $file | hexdump -v -e '" 0x" 1/1 "%02X,"' | fmt >> $out
-  echo "    }" >> $out
+  echo -e "\t}" >> $out
   echo "};" >> $out
-  echo  >> $out
+  echo >> $out
 done
 
 echo "VipsProfileFallback *vips__profile_fallback_table[] = {" >> $out
 for profile_name in $profile_names; do
-  echo "    &$profile_name," >> $out
+  echo -e "\t&$profile_name," >> $out
 done
-echo "    NULL" >> $out
+echo -e "\tNULL" >> $out
 echo "};" >> $out
