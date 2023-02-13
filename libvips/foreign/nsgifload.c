2acfe2b2793232175a2d85082df7caaf875e3c78
MathemanFlo <floh.heinrich@gmail.com>
2023-02-13 08:46:56 +0100

    Support bits per sample metadata for gif

--- libvips/foreign/nsgifload.c
+++ libvips/foreign/nsgifload.c
@@ -334,6 +334,8 @@ vips_foreign_load_nsgif_set_header( VipsForeignLoadNsgif *gif,
 
 	vips_image_set_int( image, "palette-bit-depth", 
 		ceil( log2( colours ) ) ); 
+	vips_image_set_int( image, VIPS_META_BITS_PER_SAMPLE, 
+		ceil( log2( colours ) ) ); 
 
 	/* Let our caller know if the GIF is interlaced.
 	 */
