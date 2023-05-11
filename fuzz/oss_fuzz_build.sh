69729b9dda503aa660739fd2c757db6204aca001
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-05-11 11:38:31 +0200

    OSS-Fuzz: mark `heif_deinit()` as dtor (#3489)
    Ensures the 'static' conversion tables are properly cleaned up.

--- fuzz/oss_fuzz_build.sh
+++ fuzz/oss_fuzz_build.sh
@@ -67,6 +67,8 @@ popd
 pushd $SRC/libheif
 # Ensure libvips finds heif_image_handle_get_raw_color_profile
 sed -i '/^Libs.private:/s/-lstdc++/-lc++/' libheif.pc.in
+# Ensure heif_deinit is called at program exit
+sed -i '/void heif_deinit()/i __attribute__((destructor))' libheif/init.cc
 cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_INSTALL_PREFIX=$WORK \
