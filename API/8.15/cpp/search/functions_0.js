fa508983748a2af62e29d3f3aaa182643250cb1e
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-10-11 12:33:04 +0200

    Update docs for 8.15

---
+++ API/8.15/cpp/search/functions_0.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['abs_0',['abs',['../classVImage.html#aa76c1e9860e8ea0c12da4af79c58cef0',1,'VImage']]],
+  ['acos_1',['acos',['../classVImage.html#aa4b0f6d490c521504f0346a54a942e5b',1,'VImage']]],
+  ['acosh_2',['acosh',['../classVImage.html#afa9ac8da552ab7390f74f88bb7798194',1,'VImage']]],
+  ['add_3',['add',['../classVImage.html#a79bdf3b473bfef9466ab11c68a187e6b',1,'VImage']]],
+  ['addr_4',['addr',['../classVRegion.html#a530d1e914d6780d44f055455b02430de',1,'VRegion::addr() const'],['../classVRegion.html#a1caade01a094f1dec2a66fcaa1fb8b17',1,'VRegion::addr(size_t i) const'],['../classVRegion.html#abbc5ab3aeee0e8004e42799a28d37a7f',1,'VRegion::addr(int x, int y) const']]],
+  ['affine_5',['affine',['../classVImage.html#a411ec332fdb5a4b5a9b63ea311533cb3',1,'VImage']]],
+  ['analyzeload_6',['analyzeload',['../classVImage.html#abbe512c47732118c79450ccaa455678b',1,'VImage']]],
+  ['arrayjoin_7',['arrayjoin',['../classVImage.html#a5d8d068739218fbaa5d77429678d2e4b',1,'VImage']]],
+  ['asin_8',['asin',['../classVImage.html#a8dbe6a49c901096ff60f90f3c39a8806',1,'VImage']]],
+  ['asinh_9',['asinh',['../classVImage.html#af15a507374a6eadcae840cba5d280d31',1,'VImage']]],
+  ['atan_10',['atan',['../classVImage.html#a891410666edd5eeeb7d677cc3b459d97',1,'VImage']]],
+  ['atan2_11',['atan2',['../classVImage.html#a0992276186cab49c69a521a985dedf35',1,'VImage::atan2(VImage other, VOption *options=nullptr) const'],['../classVImage.html#a5639f34e8adb03f3e40ef9e206a7c409',1,'VImage::atan2(double other, VOption *options=nullptr) const'],['../classVImage.html#abad4d92dd1ed9319e730ccc142cb1ccd',1,'VImage::atan2(std::vector&lt; double &gt; other, VOption *options=nullptr) const']]],
+  ['atanh_12',['atanh',['../classVImage.html#a47969b8cab378415c52276b74e79e815',1,'VImage']]],
+  ['autorot_13',['autorot',['../classVImage.html#a69606640ab8c0a9212cde43465422154',1,'VImage']]],
+  ['avg_14',['avg',['../classVImage.html#a569c59898c6d2ba54ce6488cbd4b71b6',1,'VImage']]]
+];
---
+++ API/8.15/cpp/search/functions_c.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['new_5ffrom_5fblob_0',['new_from_blob',['../classVSource.html#a07daa8427dd74667342124b8b15db541',1,'VSource']]],
+  ['new_5ffrom_5fbuffer_1',['new_from_buffer',['../classVImage.html#a8b4754d4a03e711331989351a82ffc7c',1,'VImage::new_from_buffer(const void *buf, size_t len, const char *option_string, VOption *options=nullptr)'],['../classVImage.html#a4f3143c5a1f8197be4b56e9613611262',1,'VImage::new_from_buffer(const std::string &amp;buf, const char *option_string, VOption *options=nullptr)']]],
+  ['new_5ffrom_5fdescriptor_2',['new_from_descriptor',['../classVSource.html#adf2fe46aabbb8b2ea4a5d5ab9bd9d811',1,'VSource']]],
+  ['new_5ffrom_5ffile_3',['new_from_file',['../classVSource.html#a350b2e65aff9da5ee0c9305175f5b51d',1,'VSource::new_from_file()'],['../classVImage.html#a2df6b3e9da75e6fc5ad1295871c56b7f',1,'VImage::new_from_file(const char *name, VOption *options=nullptr)']]],
+  ['new_5ffrom_5fimage_4',['new_from_image',['../classVImage.html#a75ebb9f9eadc50d4a10633f6040b18d1',1,'VImage::new_from_image(std::vector&lt; double &gt; pixel) const'],['../classVImage.html#a4277c021d3ab980773cc42a5f715ee4e',1,'VImage::new_from_image(double pixel) const'],['../classVRegion.html#ac925554f21b2777071c23d50a11a2245',1,'VRegion::new_from_image()']]],
+  ['new_5ffrom_5fmemory_5',['new_from_memory',['../classVSource.html#aa000ad353310aa6f35f3497397ccdeba',1,'VSource::new_from_memory()'],['../classVImage.html#a07226233b46f74f73956a4003609a06f',1,'VImage::new_from_memory(void *data, size_t size, int width, int height, int bands, VipsBandFormat format)']]],
+  ['new_5ffrom_5fmemory_5fcopy_6',['new_from_memory_copy',['../classVImage.html#a4528866ca838d28c00e8220c9ef3bb89',1,'VImage']]],
+  ['new_5ffrom_5fmemory_5fsteal_7',['new_from_memory_steal',['../classVImage.html#a9abb3d681b24bfcbce03430ad0758f45',1,'VImage']]],
+  ['new_5ffrom_5fname_8',['new_from_name',['../classVInterpolate.html#a317a8564848ddfed04ef997ab6ee1796',1,'VInterpolate']]],
+  ['new_5ffrom_5foptions_9',['new_from_options',['../classVSource.html#a0e7c2caba1dc2892a69b5843c7b161df',1,'VSource']]],
+  ['new_5ffrom_5fsource_10',['new_from_source',['../classVImage.html#ac95b11f2c9ea78dd61b11cfe58dcd6e9',1,'VImage']]],
+  ['new_5fmatrix_11',['new_matrix',['../classVImage.html#ac4166259736c14dbd799b7ca98c5a72d',1,'VImage::new_matrix(int width, int height)'],['../classVImage.html#aae32b79d8c2486e73bef9224e12b75fe',1,'VImage::new_matrix(int width, int height, double *array, int size)']]],
+  ['new_5fmatrixv_12',['new_matrixv',['../classVImage.html#a0cb544484d4c502849dcaf68317fc570',1,'VImage']]],
+  ['new_5fmemory_13',['new_memory',['../classVImage.html#a2be887dc013176d0883f0cc15c352e1f',1,'VImage']]],
+  ['new_5ftemp_5ffile_14',['new_temp_file',['../classVImage.html#abf6e5f87e5f8bdb483c30e15a47af133',1,'VImage']]],
+  ['new_5fto_5fdescriptor_15',['new_to_descriptor',['../classVTarget.html#ae1bca9ec4e8fced177d1eef974f0b02d',1,'VTarget']]],
+  ['new_5fto_5ffile_16',['new_to_file',['../classVTarget.html#a0cf29725e2d377ac599b13b7a2f6bf33',1,'VTarget']]],
+  ['new_5fto_5fmemory_17',['new_to_memory',['../classVTarget.html#a5f1959ad70ba41cb7fe397760139cbb3',1,'VTarget']]],
+  ['niftiload_18',['niftiload',['../classVImage.html#a6e695e00980367c4fac203482361f026',1,'VImage']]],
+  ['niftiload_5fsource_19',['niftiload_source',['../classVImage.html#a410df7967c6796a2757b00f5bf1ddae1',1,'VImage']]],
+  ['niftisave_20',['niftisave',['../classVImage.html#a025ad640e016f19d1cddb20ea65c7d98',1,'VImage']]]
+];
