95cdc5be014d70b3b4a06aa0267f41894fb9e1d0
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-10-28 14:39:10 +0200

    Re-add TTF sniffer
    To ensure that TTFs are not being caught by the TGA sniffer, see:
    d3f77e4433435b9770afc3a615e22b7ac219867c

--- libvips/foreign/magick.c
+++ libvips/foreign/magick.c
@@ -79,6 +79,14 @@
 static const char *
 magick_sniff(const unsigned char *bytes, size_t length)
 {
+	if (length >= 5 &&
+		bytes[0] == 0 &&
+		bytes[1] == 1 &&
+		bytes[2] == 0 &&
+		bytes[3] == 0 &&
+		bytes[4] == 0)
+		return "TTF";
+
 	if (length >= 6 &&
 		bytes[0] == 0 &&
 		bytes[1] == 0 &&
