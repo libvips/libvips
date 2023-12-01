fb1857f7e5697e9af53d27af4633b268b8aed46f
Ionen Wolkens <sudinave@gmail.com>
2023-12-01 12:06:11 -0500

    Sync vips__jpeg_region_write_target definitions (#3772)
    Prevents a build failure in the off-chance jpeg was disabled.
    
    (cherry picked from commit 70462830dc5c44e7b1d9f18f32cc0cf3c9d86908)

--- libvips/foreign/vips2jpeg.c
+++ libvips/foreign/vips2jpeg.c
@@ -1035,7 +1035,7 @@ vips__jpeg_region_write_target(VipsRegion *region, VipsRect *rect,
 	VipsTarget *target,
 	int Q, const char *profile,
 	gboolean optimize_coding, gboolean progressive,
-	gboolean strip, gboolean trellis_quant,
+	VipsForeignKeep keep, gboolean trellis_quant,
 	gboolean overshoot_deringing, gboolean optimize_scans,
 	int quant_table, VipsForeignSubsample subsample_mode,
 	int restart_interval)
