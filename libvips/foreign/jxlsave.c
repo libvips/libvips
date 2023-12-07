cdf2d27b30bbb4988ca76b742003aa61be9bcd1e
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-12-07 10:19:19 +0100

    jxlsave: prefer use of `JxlEncoderDistanceFromQuality`

--- libvips/foreign/jxlsave.c
+++ libvips/foreign/jxlsave.c
@@ -243,13 +243,16 @@ vips_foreign_save_jxl_build(VipsObject *object)
 		return -1;
 
 	/* If Q is set and distance is not, use Q to set a rough distance
-	 * value. Formula stolen from cjxl.c and very roughly approximates
-	 * libjpeg values.
+	 * value.
 	 */
 	if (!vips_object_argument_isset(object, "distance"))
+#ifdef HAVE_LIBJXL_0_9
+		jxl->distance = JxlEncoderDistanceFromQuality((float) jxl->Q);
+#else
 		jxl->distance = jxl->Q >= 30
 			? 0.1 + (100 - jxl->Q) * 0.09
 			: 53.0 / 3000.0 * jxl->Q * jxl->Q - 23.0 / 20.0 * jxl->Q + 25.0;
+#endif
 
 	/* Distance 0 is lossless. libjxl will fail for lossy distance 0.
 	 */
