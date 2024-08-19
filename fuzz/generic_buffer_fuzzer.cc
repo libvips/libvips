65a1371fb6e999ffab2ae5fc2132e851d5485a7f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-08-19 13:19:43 +0200

    OSS-Fuzz: expand coverage of buffer save fuzzers (#4092)
    Ensures that the following buffer savers are covered in OSS-Fuzz:
    `csvsave`, `heifsave`, `matrixsave`, `ppmsave`, `radsave`, `rawsave`
    and `tiffsave`.

---
+++ fuzz/generic_buffer_fuzzer.cc
@@ -0,0 +1,62 @@
+#include <vips/vips.h>
+
+/*
+#define DEBUG
+ */
+
+#ifndef SAVE_SUFFIX
+#define SAVE_SUFFIX ".jpg"
+#endif
+
+extern "C" int
+LLVMFuzzerInitialize(int *argc, char ***argv)
+{
+	vips_concurrency_set(1);
+
+#ifdef DEBUG
+	printf("available suffixes:\n");
+	GSList *suffixes = nullptr;
+	char **array = vips_foreign_get_suffixes();
+	for (int i = 0; array[i] != nullptr; i++) {
+		if (!g_slist_find_custom(suffixes, array[i],
+			(GCompareFunc) g_strcmp0)) {
+			printf("%s\n", array[i]);
+			suffixes = g_slist_append(suffixes, g_strdup(array[i]));
+		}
+
+		g_free(array[i]);
+	}
+	g_free(array);
+	g_slist_free_full(suffixes, g_free);
+#endif
+
+	return 0;
+}
+
+extern "C" int
+LLVMFuzzerTestOneInput(const guint8 *data, size_t size)
+{
+	VipsImage *image;
+	void *buf;
+	size_t len;
+
+	if (!(image = vips_image_new_from_buffer(data, size, "", nullptr)))
+		return 0;
+
+	if (image->Xsize > 100 ||
+		image->Ysize > 100 ||
+		image->Bands > 4) {
+		g_object_unref(image);
+		return 0;
+	}
+
+	if (vips_image_write_to_buffer(image, SAVE_SUFFIX, &buf, &len, nullptr)) {
+		g_object_unref(image);
+		return 0;
+	}
+
+	g_free(buf);
+	g_object_unref(image);
+
+	return 0;
+}
