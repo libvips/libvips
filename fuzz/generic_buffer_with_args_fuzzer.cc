0c80427af748f16a1daa085c79586da3670e3764
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-08-22 21:56:17 +0200

    Simplify

--- fuzz/generic_buffer_with_args_fuzzer.cc
+++ fuzz/generic_buffer_with_args_fuzzer.cc
@@ -1,5 +1,7 @@
 #include <vips/vips.h>
 
+#define MAX_ARG_LEN 4096 // =VIPS_PATH_MAX
+
 extern "C" int
 LLVMFuzzerInitialize(int *argc, char ***argv)
 {
@@ -7,20 +9,18 @@ LLVMFuzzerInitialize(int *argc, char ***argv)
 	return 0;
 }
 
-static
-const char* extractNextLine(const guint8 **data, size_t *size) {
-	char* extracted_string = nullptr;
-	for(size_t i = 0; i < (*size); i++) {
-		if((*data)[i] == '\n') {
-			extracted_string = (char *)g_malloc(i + 1);
-			memcpy(extracted_string, *data, i);
-			extracted_string[i] = '\0';
-			*data = (*data) + i + 1;
-			*size = (*size) - i - 1;
-			break;
-		}
-	}
-	return (const char*)extracted_string;
+static char *
+ExtractLine(const guint8 *data, size_t size, size_t *n)
+{
+	const guint8 *end;
+
+	end = static_cast<const guint8 *>(
+		memchr(data, '\n', VIPS_MIN(size, MAX_ARG_LEN)));
+	if (end == nullptr)
+		return nullptr;
+
+	*n = end - data;
+	return g_strndup(reinterpret_cast<const char *>(data), *n);
 }
 
 extern "C" int
@@ -28,44 +28,50 @@ LLVMFuzzerTestOneInput(const guint8 *data, size_t size)
 {
 	VipsImage *image;
 	void *buf;
-	const char *option_string, *suffix;
-	size_t len;
+	char *option_string, *suffix;
+	size_t len, n;
 
-	option_string = extractNextLine(&data, &size);
-	if (!option_string) {
+	option_string = ExtractLine(data, size, &n);
+	if (option_string == nullptr)
+		return 0;
+
+	data += n + 1;
+	size -= n + 1;
+
+	suffix = ExtractLine(data, size, &n);
+	if (suffix == nullptr) {
+		g_free(option_string);
 		return 0;
 	}
-	suffix = extractNextLine(&data, &size);
-	if (!suffix) {
-		g_free((void*)option_string);
-		return 0;
-	}
+
+	data += n + 1;
+	size -= n + 1;
 
 	if (!(image = vips_image_new_from_buffer(data, size, option_string, nullptr))) {
-		g_free((void*)option_string);
-		g_free((void*)suffix);
+		g_free(option_string);
+		g_free(suffix);
 		return 0;
 	}
+
+	// We're done with option_string, free early.
+	g_free(option_string);
 
 	if (image->Xsize > 100 ||
 		image->Ysize > 100 ||
 		image->Bands > 4) {
 		g_object_unref(image);
-		g_free((void*)option_string);
-		g_free((void*)suffix);
+		g_free(suffix);
 		return 0;
 	}
 
 	if (vips_image_write_to_buffer(image, suffix, &buf, &len, nullptr)) {
 		g_object_unref(image);
-		g_free((void*)option_string);
-		g_free((void*)suffix);
+		g_free(suffix);
 		return 0;
 	}
 
 	g_free(buf);
-	g_free((void*)option_string);
-	g_free((void*)suffix);
+	g_free(suffix);
 	g_object_unref(image);
 
 	return 0;
