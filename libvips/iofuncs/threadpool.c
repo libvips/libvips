libvips/libvips

commit 9248ab75b2c92c0db7038cac29f928e369205874

use a semaphore to count workers in a pool

libvips/iofuncs/threadpool.c
@@ -255,7 +255,7 @@ typedef struct _VipsThreadpool {
 	/* The number of active threads in this pool (as a negative number, so
          * -4 means 4 active).
 	 */
-	VipsSemaphore finish;
+	VipsSemaphore n_workers;
 
 	/* Workers up this for every loop to make the main thread tick.
 	 */
@@ -399,7 +399,7 @@ vips_thread_main_loop( void *a )
 
 	/* We are exiting: tell the main thread. 
 	 */
-	vips_semaphore_up( &pool->finish );
+        vips_semaphore_upn( &pool->n_workers, 1 );
 
 	VIPS_GATE_STOP( "vips_thread_main_loop: thread" ); 
 
@@ -426,6 +426,8 @@ vips_thread_new( VipsThreadpool *pool )
 	 * owned by the correct thread.
 	 */
 
+        vips_semaphore_upn( &pool->n_workers, -1 );
+
 	if( !(worker->thread = vips_g_thread_new( "worker", 
 		vips_thread_main_loop, worker )) ) {  
 		vips_thread_free( worker );
@@ -449,7 +451,10 @@ vips_threadpool_kill_threads( VipsThreadpool *pool )
 
 		VIPS_DEBUG_MSG( "vips_threadpool_kill_threads: "
 			"killed %d threads\n", pool->max_workers );
+
 	}
+
+        vips_semaphore_downn( &pool->n_workers, 0 );
 }
 
 static void
@@ -460,7 +465,7 @@ vips_threadpool_free( VipsThreadpool *pool )
 
 	vips_threadpool_kill_threads( pool );
 	VIPS_FREEF( vips_g_mutex_free, pool->allocate_lock );
-	vips_semaphore_destroy( &pool->finish );
+	vips_semaphore_destroy( &pool->n_workers );
 	vips_semaphore_destroy( &pool->tick );
 	VIPS_FREE( pool->workers );
 	VIPS_FREE( pool );
@@ -485,7 +490,7 @@ vips_threadpool_new( VipsImage *im )
 	pool->allocate_lock = vips_g_mutex_new();
 	pool->max_workers = vips_concurrency_get();
 	pool->workers = NULL;
-	vips_semaphore_init( &pool->finish, 0, "finish" );
+	vips_semaphore_init( &pool->n_workers, 0, "n_workers" );
 	vips_semaphore_init( &pool->tick, 0, "tick" );
 	pool->error = FALSE;
 	pool->stop = FALSE;
@@ -680,10 +685,6 @@ vips_threadpool_run( VipsImage *im,
 			pool->error )
 			break;
 	}
-
-	/* Wait for them all to hit finish.
-	 */
-	vips_semaphore_downn( &pool->finish, pool->max_workers );
 
 	/* Return 0 for success.
 	 */

commit c8ee345d8b9a2fae3db5596e07f872a476a8f8ea

threadpool: fix a race condition in error handling
Fixes a regression introduced in commit 9248ab7 (PR #3105).

libvips/iofuncs/threadpool.c
@@ -470,15 +470,21 @@ vips__worker_lock(GMutex *mutex)
 }
 
 static void
+vips_threadpool_wait(VipsThreadpool *pool)
+{
+	/* Wait for them all to exit.
+	 */
+	pool->stop = TRUE;
+	vips_semaphore_downn(&pool->n_workers, 0);
+}
+
+static void
 vips_threadpool_free(VipsThreadpool *pool)
 {
 	VIPS_DEBUG_MSG("vips_threadpool_free: \"%s\" (%p)\n",
 		pool->im->filename, pool);
 
-	/* Wait for them all to exit.
-	 */
-	pool->stop = TRUE;
-	vips_semaphore_downn(&pool->n_workers, 0);
+	vips_threadpool_wait(pool);
 
 	VIPS_FREEF(vips_g_mutex_free, pool->allocate_lock);
 	vips_semaphore_destroy(&pool->n_workers);
@@ -705,12 +711,14 @@ vips_threadpool_run(VipsImage *im,
 		}
 	}
 
+	/* This will block until the last worker completes.
+	 */
+	vips_threadpool_wait(pool);
+
 	/* Return 0 for success.
 	 */
 	result = pool->error ? -1 : 0;
 
-	/* This will block until the last worker completes.
-	 */
 	vips_threadpool_free(pool);
 
 	if (!vips_image_get_concurrency(im, 0))

test/test-suite/helpers/helpers.py
@@ -33,6 +33,7 @@ SVG_FILE = os.path.join(IMAGES, "logo.svg")
 SVGZ_FILE = os.path.join(IMAGES, "logo.svgz")
 SVG_GZ_FILE = os.path.join(IMAGES, "logo.svg.gz")
 GIF_ANIM_FILE = os.path.join(IMAGES, "cogs.gif")
+GIF_ANIM_FILE_INVALID = os.path.join(IMAGES, "invalid_multiframe.gif")
 GIF_ANIM_EXPECTED_PNG_FILE = os.path.join(IMAGES, "cogs.png")
 GIF_ANIM_DISPOSE_BACKGROUND_FILE = os.path.join(IMAGES, "dispose-background.gif")
 GIF_ANIM_DISPOSE_BACKGROUND_EXPECTED_PNG_FILE = os.path.join(IMAGES, "dispose-background.png")

test/test-suite/test_foreign.py
@@ -15,6 +15,7 @@ from helpers import \
     GIF_ANIM_EXPECTED_PNG_FILE, GIF_ANIM_DISPOSE_BACKGROUND_FILE, \
     GIF_ANIM_DISPOSE_BACKGROUND_EXPECTED_PNG_FILE, \
     GIF_ANIM_DISPOSE_PREVIOUS_FILE, \
+    GIF_ANIM_FILE_INVALID, \
     GIF_ANIM_DISPOSE_PREVIOUS_EXPECTED_PNG_FILE, \
     temp_filename, assert_almost_equal_objects, have, skip_if_no, \
     TIF1_FILE, TIF2_FILE, TIF4_FILE, WEBP_LOOKS_LIKE_SVG_FILE, \
@@ -1048,6 +1049,10 @@ class TestForeign:
 
         x2 = pyvips.Image.new_from_file(GIF_ANIM_FILE, page=1, n=-1)
         assert x2.height == 4 * x1.height
+
+        with pytest.raises(pyvips.error.Error):
+            x1 = pyvips.Image.new_from_file(GIF_ANIM_FILE_INVALID, n=-1)
+            x1.avg()
 
     @skip_if_no("gifload")
     def test_gifload_animation_dispose_background(self):