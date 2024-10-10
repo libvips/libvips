57f98297f6d03884f9c41b4c143dda3f74b7a2f1
John Cupitt <jcupitt@gmail.com>
2024-10-10 17:32:14 +0100

    add 8.16 API docs

---
+++ API/8.16/cpp/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijlmnopqrstuvwxyz",
+  1: "v",
+  2: "v",
+  3: "abcdefghijlmnopqrstuvwxyz",
+  4: "i"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Pages"
+};
+
