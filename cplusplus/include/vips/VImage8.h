7cd27481a74838b18d8c9b583d72c245b0d422e9
Lovell Fuller <github@lovell.info>
2024-10-11 09:01:41 +0100

    heifsave: expose libheif/aom auto_tiles feature
    Allows the aom encoder to determine the optimum tile count based
    on the output dimensions and available threads. This can improve
    multi-core utilisation, reducing processing time and/or memory
    usage at the cost of slightly increased file sizes.

--- cplusplus/include/vips/VImage8.h
+++ cplusplus/include/vips/VImage8.h
@@ -3446,6 +3446,7 @@ public:
 	 *   - **effort** -- CPU effort, int.
 	 *   - **subsample_mode** -- Select chroma subsample operation mode, VipsForeignSubsample.
 	 *   - **encoder** -- Select encoder to use, VipsForeignHeifEncoder.
+	 *   - **auto_tiles** -- Determine optimum tile count, bool.
 	 *   - **keep** -- Which metadata to retain, VipsForeignKeep.
 	 *   - **background** -- Background value, std::vector<double>.
 	 *   - **page_height** -- Set page height for multipage save, int.
@@ -3467,6 +3468,7 @@ public:
 	 *   - **effort** -- CPU effort, int.
 	 *   - **subsample_mode** -- Select chroma subsample operation mode, VipsForeignSubsample.
 	 *   - **encoder** -- Select encoder to use, VipsForeignHeifEncoder.
+	 *   - **auto_tiles** -- Determine optimum tile count, bool.
 	 *   - **keep** -- Which metadata to retain, VipsForeignKeep.
 	 *   - **background** -- Background value, std::vector<double>.
 	 *   - **page_height** -- Set page height for multipage save, int.
@@ -3488,6 +3490,7 @@ public:
 	 *   - **effort** -- CPU effort, int.
 	 *   - **subsample_mode** -- Select chroma subsample operation mode, VipsForeignSubsample.
 	 *   - **encoder** -- Select encoder to use, VipsForeignHeifEncoder.
+	 *   - **auto_tiles** -- Determine optimum tile count, bool.
 	 *   - **keep** -- Which metadata to retain, VipsForeignKeep.
 	 *   - **background** -- Background value, std::vector<double>.
 	 *   - **page_height** -- Set page height for multipage save, int.
