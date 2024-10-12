libvips/libvips

commit 71d356b259e7ba8a4efc2edf1d1200e823d81e82

Make `vips7compat.h` part of `vips.h` (#4197)
This automatically includes `vips7compat.h` unless building with
`-Ddeprecated=false`, preventing unexpected API breaks.

libvips/include/vips/vips7compat.h
@@ -34,6 +34,8 @@
 #ifndef VIPS_VIPS7COMPAT_H
 #define VIPS_VIPS7COMPAT_H
 
+#include <vips/mask.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif /*__cplusplus*/
@@ -1760,6 +1762,7 @@ void vips_vector_to_fixed_point(double *in, int *out, int n, int scale);
 
 #include <vips/dispatch.h>
 #include <vips/almostdeprecated.h>
+#include <vips/video.h>
 
 #ifdef __cplusplus
 }

commit 8b6165db5525ed9d649328fad5876400194fceb5

Fix build with orc
Regressed since 71d356b.

libvips/include/vips/vips7compat.h
@@ -1656,13 +1656,17 @@ size_t vips__get_sizeof_vipsobject(void);
 
 /* This is deprecated to make room for highway.
  */
+#ifndef OrcProgram
 typedef struct _OrcProgram {
 	/* Opaque */
 } OrcProgram;
+#endif
 
+#ifndef OrcExecutor
 typedef struct _OrcExecutor {
 	char data[808];
 } OrcExecutor;
+#endif
 
 #define VIPS_VECTOR_SOURCE_MAX (10)
 