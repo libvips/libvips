b424773ecd1636a9dec017650526ade05b6cacb1
Lovell Fuller <github@lovell.info>
2025-02-20 15:36:38 +0000

    heifload: unlimited flag removes all security limits
    Requires libheif v1.19.0+

--- libvips/foreign/heifload.c
+++ libvips/foreign/heifload.c
@@ -354,6 +354,13 @@ vips_foreign_load_heif_build(VipsObject *object)
 		heif_context_set_maximum_image_size_limit(heif->ctx,
 			heif->unlimited ? USHRT_MAX : 0x4000);
 #endif /* HAVE_HEIF_SET_MAX_IMAGE_SIZE_LIMIT */
+#ifdef HAVE_HEIF_GET_DISABLED_SECURITY_LIMITS
+		if (heif->unlimited) {
+			const struct heif_security_limits *unlimited =
+				heif_get_disabled_security_limits();
+			heif_context_set_security_limits(heif->ctx, unlimited);
+		}
+#endif /* HAVE_HEIF_GET_DISABLED_SECURITY_LIMITS */
 		error = heif_context_read_from_reader(heif->ctx,
 			heif->reader, heif, NULL);
 		if (error.code) {
