9c3781354053a9700cfd5b7d8f419decba645c8f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-03-29 08:58:38 +0100

    Clarify comment

--- doc/urlmap.js
+++ doc/urlmap.js
@@ -1,4 +1,4 @@
-// map namespaces to URLs for out dependencies
+// A map between namespaces and base URLs for their online documentation
 baseURLs = [
   ['GLib', 'https://docs.gtk.org/glib/'],
   ['GObject', 'https://docs.gtk.org/gobject/'],
