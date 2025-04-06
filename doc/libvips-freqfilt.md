aaf5be01c264595c743c49bb73035b39460acfb4
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-04-06 13:19:29 +0200

    doc: move the freqfilt SECTION

---
+++ doc/libvips-freqfilt.md
@@ -0,0 +1,16 @@
+Title: Fourier transforms and frequency-domain filters
+
+<!-- libvips/freqfilt -->
+
+# Fourier transforms and frequency-domain filters
+
+To and from Fourier space, filter in Fourier space, convert Fourier-space
+images to a displayable form.
+
+## Functions
+
+* [method@Image.fwfft]
+* [method@Image.invfft]
+* [method@Image.freqmult]
+* [method@Image.spectrum]
+* [method@Image.phasecor]
--- libvips/freqfilt/freqfilt.c
+++ libvips/freqfilt/freqfilt.c
@@ -49,17 +49,6 @@
 
 #include "pfreqfilt.h"
 
-/**
- * SECTION: freqfilt
- * @short_description: fourier transforms and frequency-domin filters
- * @stability: Stable
- * @see_also: <link linkend="VipsImage">image</link>
- * @include: vips/vips.h
- *
- * To and from Fourier space, filter in Fourier space, convert Fourier-space
- * images to a displayable form.
- */
-
 G_DEFINE_ABSTRACT_TYPE(VipsFreqfilt, vips_freqfilt, VIPS_TYPE_OPERATION);
 
 static int
