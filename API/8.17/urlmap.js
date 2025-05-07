f1c39273d48afba0b379de46c621617a57a10a85
John Cupitt <jcupitt@gmail.com>
2025-05-07 14:56:33 +0100

    add some sample 8.17 docs

---
+++ API/8.17/urlmap.js
@@ -0,0 +1,7 @@
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+  ['GLib', 'https://docs.gtk.org/glib/'],
+  ['Gio', 'https://docs.gtk.org/gio/'],
+  ['GObject', 'https://docs.gtk.org/gobject/'],
+  ['GModule', 'https://docs.gtk.org/gmodule/'],
+]
