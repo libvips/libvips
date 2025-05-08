e628d4fe47ac30c208e6ddecb048b956ed9715dc
John Cupitt <jcupitt@gmail.com>
2025-05-08 16:07:45 +0100

    Update libvips/foreign/jp2kload.c
    Co-authored-by: Kleis Auke Wolthuizen <github@kleisauke.nl>

--- libvips/foreign/jp2kload.c
+++ libvips/foreign/jp2kload.c
@@ -1068,9 +1068,6 @@ vips_foreign_load_jp2k_generate_tiled(VipsRegion *out,
 		// stop compiler warnings
 		VipsRect hit = { 0 };
 
-		// keep compilers happy
-		hit = (VipsRect) { 0 };
-
 		x = 0;
 		while (x < r->width) {
 			/* libvips tile position to opj base resolution coordinates.
