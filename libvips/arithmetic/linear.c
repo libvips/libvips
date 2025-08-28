libvips/libvips

commit 9cc54a0627b5889eba46af4c99f07ee3ce58b7f1

enable caching of vips_system() (#4640)
* enable caching of vips_system()

We were not caching vips_system() calls, since they could potentially
have side effects. However, almost all the time we are doing things
like running imagemagick commands, so caching is very helpful.

This patch enables caching of vips_system(), and adds better hashing of
compound argument types.

* vips_linear() was changing its args

the new arg hasher spotted it

improve the arg value change detector

* add a "nocache" arg to system

* add "cache" arg to system

* small polish

libvips/arithmetic/linear.c
@@ -125,14 +125,36 @@ vips_linear_build(VipsObject *object)
 
 	int i;
 
-	/* If we have a three-element vector, we need to bandup the image to
+	/* If all elements of the constants are equal, we can treat them as
+	 * a single element.
+	 */
+	int a_n = 1;
+	if (linear->a) {
+		double *ary = (double *) linear->a->data;
+
+		for (i = 1; i < linear->a->n; i++)
+			if (ary[i] != ary[0]) {
+				a_n = linear->a->n;
+				break;
+			}
+	}
+	int b_n = 1;
+	if (linear->b) {
+		double *ary = (double *) linear->b->data;
+
+		for (i = 1; i < linear->b->n; i++)
+			if (ary[i] != ary[0]) {
+				b_n = linear->b->n;
+				break;
+			}
+	}
+
+	/* If we have a $n element vector, we need to bandup the image to
 	 * match.
 	 */
 	linear->n = 1;
-	if (linear->a)
-		linear->n = VIPS_MAX(linear->n, linear->a->n);
-	if (linear->b)
-		linear->n = VIPS_MAX(linear->n, linear->b->n);
+	linear->n = VIPS_MAX(linear->n, a_n);
+	linear->n = VIPS_MAX(linear->n, b_n);
 	if (unary->in) {
 		int bands;
 
@@ -141,46 +163,10 @@ vips_linear_build(VipsObject *object)
 	}
 	arithmetic->base_bands = linear->n;
 
-	if (unary->in &&
-		linear->a &&
-		linear->b) {
-		if (vips_check_vector(class->nickname,
-				linear->a->n, unary->in) ||
-			vips_check_vector(class->nickname,
-				linear->b->n, unary->in))
+	if (unary->in) {
+		if (vips_check_vector(class->nickname, a_n, unary->in) ||
+			vips_check_vector(class->nickname, b_n, unary->in))
 			return -1;
-	}
-
-	/* If all elements of the constants are equal, we can shrink them down
-	 * to a single element.
-	 */
-	if (linear->a) {
-		double *ary = (double *) linear->a->data;
-		gboolean all_equal;
-
-		all_equal = TRUE;
-		for (i = 1; i < linear->a->n; i++)
-			if (ary[i] != ary[0]) {
-				all_equal = FALSE;
-				break;
-			}
-
-		if (all_equal)
-			linear->a->n = 1;
-	}
-	if (linear->b) {
-		double *ary = (double *) linear->b->data;
-		gboolean all_equal;
-
-		all_equal = TRUE;
-		for (i = 1; i < linear->b->n; i++)
-			if (ary[i] != ary[0]) {
-				all_equal = FALSE;
-				break;
-			}
-
-		if (all_equal)
-			linear->b->n = 1;
 	}
 
 	/* Make up-banded versions of our constants.
@@ -191,14 +177,14 @@ vips_linear_build(VipsObject *object)
 	for (i = 0; i < linear->n; i++) {
 		if (linear->a) {
 			double *ary = (double *) linear->a->data;
-			int j = VIPS_MIN(i, linear->a->n - 1);
+			int j = VIPS_MIN(i, a_n - 1);
 
 			linear->a_ready[i] = ary[j];
 		}
 
 		if (linear->b) {
 			double *ary = (double *) linear->b->data;
-			int j = VIPS_MIN(i, linear->b->n - 1);
+			int j = VIPS_MIN(i, b_n - 1);
 
 			linear->b_ready[i] = ary[j];
 		}

libvips/iofuncs/cache.c
@@ -185,7 +185,44 @@ vips_value_hash(GParamSpec *pspec, const GValue *value)
 	else if (generic == G_TYPE_PARAM_BOXED) {
 		void *p = g_value_get_boxed(value);
 
-		return p ? g_direct_hash(p) : 0;
+		// array_object is an internal type, don't have a case for this
+		if (!p)
+			return 0;
+		else if (G_VALUE_TYPE(value) == VIPS_TYPE_ARRAY_INT) {
+			int n;
+			int *array = (int *)
+				vips_area_get_data(VIPS_AREA(p), NULL, &n, NULL, NULL);
+
+			guint hash = 0;
+			for (int i = 0; i < n; i++)
+				hash = (hash << 1) ^ g_int_hash(&array[i]);
+
+			return hash;
+		}
+		else if (G_VALUE_TYPE(value) == VIPS_TYPE_ARRAY_DOUBLE) {
+			int n;
+			double *array = (double *)
+				vips_area_get_data(VIPS_AREA(p), NULL, &n, NULL, NULL);
+
+			guint hash = 0;
+			for (int i = 0; i < n; i++)
+				hash = (hash << 1) ^ g_double_hash(&array[i]);
+
+			return hash;
+		}
+		else if (G_VALUE_TYPE(value) == VIPS_TYPE_ARRAY_IMAGE) {
+			int n;
+			void **array = (void **)
+				vips_area_get_data(VIPS_AREA(p), NULL, &n, NULL, NULL);
+
+			guint hash = 0;
+			for (int i = 0; i < n; i++)
+				hash = (hash << 1) ^ g_direct_hash(array[i]);
+
+			return hash;
+		}
+		else
+			return g_direct_hash(p);
 	}
 	else if (generic == G_TYPE_PARAM_POINTER) {
 		void *p = g_value_get_pointer(value);
@@ -275,8 +312,77 @@ vips_value_equal(GParamSpec *pspec, const GValue *v1, const GValue *v2)
 		else
 			return s1 && s2 && strcmp(s1, s2) == 0;
 	}
-	if (generic == G_TYPE_PARAM_BOXED)
-		return g_value_get_boxed(v1) == g_value_get_boxed(v2);
+	if (generic == G_TYPE_PARAM_BOXED) {
+		void *p1 = g_value_get_boxed(v1);
+		void *p2 = g_value_get_boxed(v2);
+
+		if (p1 == p2)
+			return TRUE;
+		else if (!p1 || !p2)
+			return FALSE;
+		else if (t1 == VIPS_TYPE_ARRAY_INT) {
+			int n1;
+			int *array1 = (int *)
+				vips_area_get_data(VIPS_AREA(p1), NULL, &n1, NULL, NULL);
+			int n2;
+			int *array2 = (int *)
+				vips_area_get_data(VIPS_AREA(p2), NULL, &n2, NULL, NULL);
+
+			if (n1 != n2)
+				return FALSE;
+			else if (array1 == array2)
+				return TRUE;
+			else {
+				for (int i = 0; i < n1; i++)
+					if (array1[i] != array2[i])
+						return FALSE;
+
+				return TRUE;
+			}
+		}
+		else if (t1 == VIPS_TYPE_ARRAY_DOUBLE) {
+			int n1;
+			double *array1 = (double *)
+				vips_area_get_data(VIPS_AREA(p1), NULL, &n1, NULL, NULL);
+			int n2;
+			double *array2 = (double *)
+				vips_area_get_data(VIPS_AREA(p2), NULL, &n2, NULL, NULL);
+
+			if (n1 != n2)
+				return FALSE;
+			else if (array1 == array2)
+				return TRUE;
+			else {
+				for (int i = 0; i < n1; i++)
+					if (array1[i] != array2[i])
+						return FALSE;
+
+				return TRUE;
+			}
+		}
+		else if (t1 == VIPS_TYPE_ARRAY_IMAGE) {
+			int n1;
+			void **array1 = (void **)
+				vips_area_get_data(VIPS_AREA(p1), NULL, &n1, NULL, NULL);
+			int n2;
+			void **array2 = (void **)
+				vips_area_get_data(VIPS_AREA(p2), NULL, &n2, NULL, NULL);
+
+			if (n1 != n2)
+				return FALSE;
+			else if (array1 == array2)
+				return TRUE;
+			else {
+				for (int i = 0; i < n1; i++)
+					if (array1[i] != array2[i])
+						return FALSE;
+
+				return TRUE;
+			}
+		}
+		else
+			return p1 == p2;
+	}
 	if (generic == G_TYPE_PARAM_POINTER)
 		return g_value_get_pointer(v1) == g_value_get_pointer(v2);
 	if (generic == G_TYPE_PARAM_OBJECT)
@@ -815,6 +921,49 @@ vips_cache_trim(void)
 	g_mutex_unlock(&vips_cache_lock);
 }
 
+#ifdef DEBUG_LEAK
+static void *
+vips_cache_find_differences(VipsObject *object,
+	GParamSpec *pspec,
+	VipsArgumentClass *argument_class,
+	VipsArgumentInstance *argument_instance,
+	void *a, void *b)
+{
+	VipsOperation *operation_before = VIPS_OPERATION(a);
+
+	if ((argument_class->flags & VIPS_ARGUMENT_CONSTRUCT) &&
+		(argument_class->flags & VIPS_ARGUMENT_INPUT) &&
+		argument_instance->assigned) {
+		const char *name = g_param_spec_get_name(pspec);
+
+		GValue value_before = G_VALUE_INIT;
+		g_object_get_property(G_OBJECT(operation_before), name, &value_before);
+		unsigned int hash_before = vips_value_hash(pspec, &value_before);
+
+		GValue value_after = G_VALUE_INIT;
+		g_object_get_property(G_OBJECT(object), name, &value_after);
+		unsigned int hash_after = vips_value_hash(pspec, &value_after);
+
+		if (hash_before != hash_after) {
+			g_warning("arg \"%s\" has changed value during build", name);
+
+			char *str_before = g_strdup_value_contents(&value_before);
+			g_warning("\tvalue before: %s", str_before);
+			g_free(str_before);
+
+			char *str_after = g_strdup_value_contents(&value_after);
+			g_warning("\tvalue after: %s", str_after);
+			g_free(str_after);
+		}
+
+		g_value_unset(&value_before);
+		g_value_unset(&value_after);
+	}
+
+	return NULL;
+}
+#endif /*DEBUG_LEAK*/
+
 /**
  * vips_cache_operation_buildp: (skip)
  * @operation: pointer to operation to lookup
@@ -901,6 +1050,11 @@ vips_cache_operation_buildp(VipsOperation **operation)
 		 */
 		if (vips__leak) {
 			hash_before = vips_operation_hash(*operation);
+
+			/* This isn't a deep copy, so it won't detect eg.
+			 * operations modifying compound objects like VipsArrayInt.
+			 * However, the hash_before value we save will.
+			 */
 			operation_before = vips_operation_copy(*operation);
 		}
 #endif /*DEBUG_LEAK*/
@@ -912,25 +1066,14 @@ vips_cache_operation_buildp(VipsOperation **operation)
 		if (vips__leak &&
 			!(flags & VIPS_OPERATION_NOCACHE) &&
 			hash_before != vips_operation_hash(*operation)) {
-			const char *name = (const char *)
-				vips_argument_map(VIPS_OBJECT(*operation),
-					vips_object_equal_arg, operation_before, NULL);
-			VipsObject *object = VIPS_OBJECT(*operation);
-			VipsObjectClass *class = VIPS_OBJECT_GET_CLASS(object);
-
 			char txt[256];
 			VipsBuf buf = VIPS_BUF_STATIC(txt);
+			vips_object_summary(VIPS_OBJECT(operation_before), &buf);
+			g_warning("vips_cache_operation_buildp: "
+				"arg mismatch on build of: %s", vips_buf_all(&buf));
 
-			VIPS_UNREF(operation_before);
-
-			vips_object_summary_class(class, &buf);
-			vips_buf_appends(&buf, ", ");
-			vips_object_summary(object, &buf);
-			vips_buf_appends(&buf, ", ");
-			vips_error(class->nickname, "arg \"%s\" changed during build, %s",
-				name, vips_buf_all(&buf));
-
-			return -1;
+			vips_argument_map(VIPS_OBJECT(*operation),
+				vips_cache_find_differences, operation_before, NULL);
 		}
 
 		VIPS_UNREF(operation_before);

libvips/iofuncs/object.c
@@ -179,21 +179,21 @@
  * Input gobjects are automatically reffed, output gobjects automatically ref
  * us. We also automatically watch for "destroy" and unlink.
  *
- * [flags@Vips.ArgumentFlags.SET_ALWAYS] is handy for arguments which are set from C. For
- * example, [property@Image:width] is a property that gives access to the Xsize
- * member of struct _VipsImage. We default its 'assigned' to `TRUE`
- * since the field is always set directly by C.
+ * [flags@Vips.ArgumentFlags.SET_ALWAYS] is handy for arguments which are set
+ * from C. For example, [property@Image:width] is a property that gives
+ * access to the Xsize member of struct _VipsImage. We default its
+ * 'assigned' to `TRUE` since the field is always set directly by C.
  *
- * [flags@Vips.ArgumentFlags.DEPRECATED] arguments are not shown in help text, are not
- * looked for if required, are not checked for "have-been-set". You can
+ * [flags@Vips.ArgumentFlags.DEPRECATED] arguments are not shown in help text,
+ * are not looked for if required, are not checked for "have-been-set". You can
  * deprecate a required argument, but you must obviously add a new required
  * argument if you do.
  *
- * Input args with [flags@Vips.ArgumentFlags.MODIFY] will be modified by the operation.
- * This is used for things like the in-place drawing operations.
+ * Input args with [flags@Vips.ArgumentFlags.MODIFY] will be modified by the
+ * operation. This is used for things like the in-place drawing operations.
  *
- * [flags@Vips.ArgumentFlags.NON_HASHABLE] stops the argument being used in hash and
- * equality tests. It's useful for arguments like `revalidate` which
+ * [flags@Vips.ArgumentFlags.NON_HASHABLE] stops the argument being used in
+ * hash and equality tests. It's useful for arguments like `revalidate` which
  * control the behaviour of the operator cache.
  */
 

libvips/iofuncs/operation.c
@@ -186,23 +186,24 @@
  * not at the top of the image. In this case, the first part of the image will
  * be read and discarded
  *
- * [flags@Vips.OperationFlags.NOCACHE] means that the operation must not be cached by
- * vips.
+ * [flags@Vips.OperationFlags.NOCACHE] means that the operation must not be
+ * cached by vips.
  *
- * [flags@Vips.OperationFlags.DEPRECATED] means this is an old operation kept in vips for
- * compatibility only and should be hidden from users.
+ * [flags@Vips.OperationFlags.DEPRECATED] means this is an old operation kept
+ * in vips for compatibility only and should be hidden from users.
  *
- * [flags@Vips.OperationFlags.UNTRUSTED] means the operation depends on external libraries
- * which have not been hardened against attack. It should probably not be used
- * on untrusted input. Use [func@block_untrusted_set] to block all
- * untrusted operations.
+ * [flags@Vips.OperationFlags.UNTRUSTED] means the operation depends on
+ * external libraries which have not been hardened against attack. It should
+ * probably not be used on untrusted input. Use [func@block_untrusted_set]
+ * to block all untrusted operations.
  *
- * [flags@Vips.OperationFlags.BLOCKED] means the operation is prevented from executing. Use
- * [func@Operation.block_set] to enable and disable groups of operations.
+ * [flags@Vips.OperationFlags.BLOCKED] means the operation is prevented from
+ * executing. Use [func@Operation.block_set] to enable and disable groups of
+ * operations.
  *
- * [flags@Vips.OperationFlags.REVALIDATE] force the operation to run, updating the cache
- * with the new value. This is used by eg. VipsForeignLoad to implement the
- * "revalidate" argument.
+ * [flags@Vips.OperationFlags.REVALIDATE] force the operation to run, updating
+ * the cache with the new value. This is used by eg. VipsForeignLoad to
+ * implement the "revalidate" argument.
  */
 
 /* Abstract base class for operations.

libvips/iofuncs/system.c
@@ -22,6 +22,8 @@
  * 	  stray command-windows on Windows
  * 27/3/16
  * 	- allow [options] in out_format
+ * 20/8/25
+ *	- add "cache" argument
  */
 
 /*
@@ -51,6 +53,10 @@
 
  */
 
+/*
+#define DEBUG
+ */
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif /*HAVE_CONFIG_H*/
@@ -78,6 +84,7 @@ typedef struct _VipsSystem {
 	char *in_format;
 	char *out_format;
 	char *log;
+	gboolean cache;
 
 	/* Array of names we wrote the input images to.
 	 */
@@ -201,9 +208,16 @@ vips_system_build(VipsObject *object)
 			p[1] == '%')
 			memmove(p, p + 1, strlen(p));
 
+#ifdef DEBUG
+	printf("vips_system_build: spawn <<%s>> ...\n", cmd);
+#endif /*DEBUG*/
 	if (!g_spawn_command_line_sync(cmd,
 			&std_output, &std_error, &result, &error) ||
 		result) {
+#ifdef DEBUG
+		printf("\t... failed\n");
+#endif /*DEBUG*/
+
 		if (error) {
 			vips_error(class->nickname, "%s", error->message);
 			g_error_free(error);
@@ -227,6 +241,10 @@ vips_system_build(VipsObject *object)
 
 		return -1;
 	}
+
+#ifdef DEBUG
+	printf("\t... success\n");
+#endif /*DEBUG*/
 
 	if (std_error) {
 		g_strchomp(std_error);
@@ -254,6 +272,19 @@ vips_system_build(VipsObject *object)
 	return 0;
 }
 
+static VipsOperationFlags
+vips_system_get_flags(VipsOperation *operation)
+{
+	VipsOperationFlags flags =
+		VIPS_OPERATION_CLASS(vips_system_parent_class)->get_flags(operation);
+	VipsSystem *system = (VipsSystem *) operation;
+
+	if (system->cache)
+		flags &= ~VIPS_OPERATION_NOCACHE;
+
+	return flags;
+}
+
 static void
 vips_system_class_init(VipsSystemClass *class)
 {
@@ -269,9 +300,8 @@ vips_system_class_init(VipsSystemClass *class)
 	vobject_class->description = _("run an external command");
 	vobject_class->build = vips_system_build;
 
-	/* Commands can have side-effects, so don't cache them.
-	 */
-	operation_class->flags |= VIPS_OPERATION_NOCACHE;
+	operation_class->get_flags = vips_system_get_flags;
+	operation_class->flags = VIPS_OPERATION_NOCACHE;
 
 	VIPS_ARG_BOXED(class, "in", 0,
 		_("Input"),
@@ -293,26 +323,34 @@ vips_system_class_init(VipsSystemClass *class)
 		G_STRUCT_OFFSET(VipsSystem, cmd_format),
 		NULL);
 
-	VIPS_ARG_STRING(class, "in_format", 2,
+	VIPS_ARG_STRING(class, "in_format", 3,
 		_("Input format"),
 		_("Format for input filename"),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET(VipsSystem, in_format),
 		NULL);
 
-	VIPS_ARG_STRING(class, "out_format", 2,
+	VIPS_ARG_STRING(class, "out_format", 4,
 		_("Output format"),
 		_("Format for output filename"),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET(VipsSystem, out_format),
 		NULL);
 
-	VIPS_ARG_STRING(class, "log", 2,
+	VIPS_ARG_BOOL(class, "cache", 5,
+		_("Cache"),
+		_("Cache this call"),
+		VIPS_ARGUMENT_OPTIONAL_INPUT,
+		G_STRUCT_OFFSET(VipsSystem, cache),
+		FALSE);
+
+	VIPS_ARG_STRING(class, "log", 6,
 		_("Log"),
 		_("Command log"),
 		VIPS_ARGUMENT_OPTIONAL_OUTPUT,
 		G_STRUCT_OFFSET(VipsSystem, log),
 		NULL);
+
 }
 
 static void
@@ -342,7 +380,8 @@ vips_system_init(VipsSystem *system)
  * You can put a number between the `%` and the `s` to change the order
  * in which the substitution occurs.
  *
- * The command is executed with [`popen()`](man:popen(3)) and the output captured in @log.
+ * The command is executed with [`popen()`](man:popen(3)) and the output
+ * captured in @log.
  *
  * After the command finishes, if @out_format is set, the output image is
  * opened and returned in @out. You can append `[options]` to @out_format to
@@ -351,6 +390,9 @@ vips_system_init(VipsSystem *system)
  * Closing @out image will automatically delete the output file.
  *
  * Finally the input images are deleted.
+ *
+ * If @cache is set, this call will be added to the libvips operation cache
+ * and reused if possible.
  *
  * For example, this call will run the ImageMagick convert program on an
  * image, using JPEG files to pass images into and out of the convert command.
@@ -365,6 +407,7 @@ vips_system_init(VipsSystem *system)
  *         "out", &out,
  *         "in_format", "%s.jpg",
  *         "out_format", "%s.jpg",
+ *         "cache", TRUE,
  *         "log", &log,
  *         NULL))
  *     error ...
@@ -375,6 +418,7 @@ vips_system_init(VipsSystem *system)
  *     * @out: [class@Image], output, image
  *     * @in_format: `gchararray`, write input files like this
  *     * @out_format: `gchararray`, write output filename like this
+ *     * @cache: `gboolean`, cache this call
  *     * @log: `gchararray`, output, stdout of command is returned here
  *
  * Returns: 0 on success, -1 on failure.

libvips/iofuncs/type.c
@@ -336,8 +336,9 @@ vips_area_free_array_object(GObject **array, VipsArea *area)
  * vips_area_new_array_object: (constructor)
  * @n: number of elements in the array
  *
- * An area which holds an array of [class@GObject.Object] s. See [ctor@Area.new_array]. When
- * the area is freed, each [class@GObject.Object] will be unreffed.
+ * An area which holds an array of [class@GObject.Object] s. See
+ * [ctor@Area.new_array]. When the area is freed, each [class@GObject.Object]
+ * will be unreffed.
  *
  * Add an extra `NULL` element at the end, handy for eg.
  * [func@Image.pipeline_array] etc.
@@ -1932,7 +1933,8 @@ vips_value_get_array_object(const GValue *value, int *n)
  * @value: (out): [struct@GObject.Value] to set
  * @n: the number of elements
  *
- * Set @value to hold an array of [class@GObject.Object]. Pass in the array length in @n.
+ * Set @value to hold an array of [class@GObject.Object]. Pass in the array
+ * length in @n.
  *
  * ::: seealso
  *     [func@value_get_array_object].

commit 61ebfe7bf91e270c6e09eb9ea796f3ed4bb02162

linear: avoid single-band image when all constants are equal
Fixes a regression introduced in commit 9cc54a0.

libvips/arithmetic/linear.c
@@ -125,6 +125,30 @@ vips_linear_build(VipsObject *object)
 
 	int i;
 
+	/* If we have a $n element vector, we need to bandup the image to
+	 * match.
+	 */
+	linear->n = 1;
+	if (linear->a)
+		linear->n = VIPS_MAX(linear->n, linear->a->n);
+	if (linear->b)
+		linear->n = VIPS_MAX(linear->n, linear->b->n);
+	if (unary->in) {
+		int bands;
+
+		vips_image_decode_predict(unary->in, &bands, NULL);
+		linear->n = VIPS_MAX(linear->n, bands);
+	}
+	arithmetic->base_bands = linear->n;
+
+	if (unary->in &&
+		linear->a &&
+		linear->b) {
+		if (vips_check_vector(class->nickname, linear->a->n, unary->in) ||
+			vips_check_vector(class->nickname, linear->b->n, unary->in))
+			return -1;
+	}
+
 	/* If all elements of the constants are equal, we can treat them as
 	 * a single element.
 	 */
@@ -147,26 +171,6 @@ vips_linear_build(VipsObject *object)
 				b_n = linear->b->n;
 				break;
 			}
-	}
-
-	/* If we have a $n element vector, we need to bandup the image to
-	 * match.
-	 */
-	linear->n = 1;
-	linear->n = VIPS_MAX(linear->n, a_n);
-	linear->n = VIPS_MAX(linear->n, b_n);
-	if (unary->in) {
-		int bands;
-
-		vips_image_decode_predict(unary->in, &bands, NULL);
-		linear->n = VIPS_MAX(linear->n, bands);
-	}
-	arithmetic->base_bands = linear->n;
-
-	if (unary->in) {
-		if (vips_check_vector(class->nickname, a_n, unary->in) ||
-			vips_check_vector(class->nickname, b_n, unary->in))
-			return -1;
 	}
 
 	/* Make up-banded versions of our constants.

test/test-suite/test_iofuncs.py
@@ -35,6 +35,10 @@ class TestIofuncs:
         assert im2.bands == 3
         assert im2.avg() == 2
 
+        im2 = im.new_from_image([0, 0, 0, 0])
+
+        assert im2.bands == 4
+
     def test_new_from_memory(self):
         s = bytearray(200)
         im = pyvips.Image.new_from_memory(s, 20, 10, 1, 'uchar')