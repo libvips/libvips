a1f8efc8f603f2dbd38acf81e965647c2cd78baf
Lovell Fuller <github@lovell.info>
2025-08-29 13:57:14 +0100

    tiffload: ensure processing halts for memory-related errors
    - when fail_on is set to error level or higher
    - when message text matches known-fatal conditions

--- libvips/foreign/tiff2vips.c
+++ libvips/foreign/tiff2vips.c
@@ -612,6 +612,19 @@ static int
 rtiff_handler_error(TIFF *tiff, void* user_data,
 	const char *module, const char *fmt, va_list ap)
 {
+	if (user_data) {
+		Rtiff *rtiff = (Rtiff*) user_data;
+		if (rtiff->fail_on >= VIPS_FAIL_ON_ERROR) {
+			rtiff->failed = TRUE;
+		} else if (fmt) {
+			/* Inspect message for always-fatal errors.
+			*/
+			if (strncmp(fmt, "Cumulated memory", 16) == 0) {
+				rtiff->failed = TRUE;
+			}
+		}
+	}
+
 	vips_verror("tiff2vips", fmt, ap);
 	return 1;
 }
