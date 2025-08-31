c7a53c809706625f73558bca1a2308f79745ea73
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-08-31 10:49:03 +0200

    linear: fix fast-path when all constants are equal

--- libvips/arithmetic/linear.c
+++ libvips/arithmetic/linear.c
@@ -103,9 +103,12 @@ typedef struct _VipsLinear {
 	 */
 	gboolean uchar;
 
+	/* TRUE if a and b each contain only one unique value.
+	 */
+	gboolean single_element;
+
 	/* Our constants expanded to match arith->ready in size.
 	 */
-	int n;
 	double *a_ready;
 	double *b_ready;
 
@@ -128,18 +131,18 @@ vips_linear_build(VipsObject *object)
 	/* If we have a $n element vector, we need to bandup the image to
 	 * match.
 	 */
-	linear->n = 1;
+	int n = 1;
 	if (linear->a)
-		linear->n = VIPS_MAX(linear->n, linear->a->n);
+		n = VIPS_MAX(n, linear->a->n);
 	if (linear->b)
-		linear->n = VIPS_MAX(linear->n, linear->b->n);
+		n = VIPS_MAX(n, linear->b->n);
 	if (unary->in) {
 		int bands;
 
 		vips_image_decode_predict(unary->in, &bands, NULL);
-		linear->n = VIPS_MAX(linear->n, bands);
+		n = VIPS_MAX(n, bands);
 	}
-	arithmetic->base_bands = linear->n;
+	arithmetic->base_bands = n;
 
 	if (unary->in &&
 		linear->a &&
@@ -173,12 +176,14 @@ vips_linear_build(VipsObject *object)
 			}
 	}
 
+	linear->single_element = a_n == 1 && b_n == 1;
+
 	/* Make up-banded versions of our constants.
 	 */
-	linear->a_ready = VIPS_ARRAY(linear, linear->n, double);
-	linear->b_ready = VIPS_ARRAY(linear, linear->n, double);
+	linear->a_ready = VIPS_ARRAY(linear, n, double);
+	linear->b_ready = VIPS_ARRAY(linear, n, double);
 
-	for (i = 0; i < linear->n; i++) {
+	for (i = 0; i < n; i++) {
 		if (linear->a) {
 			double *ary = (double *) linear->a->data;
 			int j = VIPS_MIN(i, a_n - 1);
@@ -231,7 +236,7 @@ vips_linear_build(VipsObject *object)
 
 #define LOOP(IN, OUT) \
 	{ \
-		if (linear->a->n == 1 && linear->b->n == 1) { \
+		if (linear->single_element) { \
 			LOOP1(IN, OUT); \
 		} \
 		else { \
@@ -290,7 +295,7 @@ vips_linear_build(VipsObject *object)
 
 #define LOOPuc(IN) \
 	{ \
-		if (linear->a->n == 1 && linear->b->n == 1) { \
+		if (linear->single_element) { \
 			LOOP1uc(IN); \
 		} \
 		else { \
