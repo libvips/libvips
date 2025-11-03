9cd0f6ed0bd8b4df8169ae43068ebf757b6793eb
John Cupitt <jcupitt@gmail.com>
2025-11-03 11:20:28 +0000

    call terminateprocess to exit
    clang on win10 can generate exes which occasionally deadlock on process
    exit, see https://github.com/llvm/llvm-project/issues/110954
    
    on windows, call TerminateProcess instead to bypass orderly shutdown

--- tools/vips.c
+++ tools/vips.c
@@ -930,5 +930,6 @@ main(int argc, char **argv)
 
 	vips_shutdown();
 
+	vips__win32_terminate(0);
 	return 0;
 }
--- tools/vipsthumbnail.c
+++ tools/vipsthumbnail.c
@@ -592,5 +592,6 @@ main(int argc, char **argv)
 
 	vips_shutdown();
 
+	vips__win32_terminate(result);
 	return result;
 }
