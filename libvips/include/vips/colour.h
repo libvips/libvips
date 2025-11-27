074c3923893b22b0df42e76ad086784638e118a3
John Cupitt <jcupitt@gmail.com>
2025-11-27 17:59:05 +0000

    Update libvips/include/vips/colour.h
    Co-authored-by: Kleis Auke Wolthuizen <github@kleisauke.nl>

--- libvips/include/vips/colour.h
+++ libvips/include/vips/colour.h
@@ -205,6 +205,7 @@ int vips_XYZ2CMYK(VipsImage *in, VipsImage **out, ...)
 
 VIPS_API
 int vips_uhdr2scRGB(VipsImage *in, VipsImage **out, ...);
+	G_GNUC_NULL_TERMINATED
 
 VIPS_API
 int vips_profile_load(const char *name, VipsBlob **profile, ...)
