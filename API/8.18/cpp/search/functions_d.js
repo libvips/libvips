cbcf6b1ff3ea49e756abc31b81026d16143424d5
John Cupitt <jcupitt@gmail.com>
2025-12-06 18:09:40 +0000

    update again for recent changes

--- API/8.18/cpp/search/functions_d.js
+++ API/8.18/cpp/search/functions_d.js
@@ -1,10 +1,13 @@
 var searchData=
 [
-  ['openexrload_0',['openexrload',['../classVImage.html#ab1613145f92fd4a16f21b23991335f32',1,'VImage']]],
-  ['openslideload_1',['openslideload',['../classVImage.html#a782970cd2e26ffc754ba473b86e6efdc',1,'VImage']]],
-  ['openslideload_5fsource_2',['openslideload_source',['../classVImage.html#aee8573bac9442a2d24f22978d4365546',1,'VImage']]],
-  ['operator_28_29_3',['operator()',['../classVRegion.html#a70113876aea0deec50a8edbed4b266a5',1,'VRegion']]],
-  ['operator_5b_5d_4',['operator[]',['../classVRegion.html#ab0fd17162b84a27d114b177b56c895a6',1,'VRegion']]],
-  ['option_5',['option',['../classVImage.html#af432280245e5c33bd800aa4a9983ea22',1,'VImage']]],
-  ['ostream_5fprint_6',['ostream_print',['../classVError.html#a8b2824da270e4d493b24fa48097165b5',1,'VError']]]
+  ['oklab2oklch_0',['Oklab2Oklch',['../classVImage.html#af842719ef0ceef4ef049877c6a2f457a',1,'VImage']]],
+  ['oklab2xyz_1',['Oklab2XYZ',['../classVImage.html#a43501691ed758e90191d814bc19fb44e',1,'VImage']]],
+  ['oklch2oklab_2',['Oklch2Oklab',['../classVImage.html#ab85770394d483063d288ff7b12594423',1,'VImage']]],
+  ['openexrload_3',['openexrload',['../classVImage.html#ab1613145f92fd4a16f21b23991335f32',1,'VImage']]],
+  ['openslideload_4',['openslideload',['../classVImage.html#a782970cd2e26ffc754ba473b86e6efdc',1,'VImage']]],
+  ['openslideload_5fsource_5',['openslideload_source',['../classVImage.html#aee8573bac9442a2d24f22978d4365546',1,'VImage']]],
+  ['operator_28_29_6',['operator()',['../classVRegion.html#a70113876aea0deec50a8edbed4b266a5',1,'VRegion']]],
+  ['operator_5b_5d_7',['operator[]',['../classVRegion.html#ab0fd17162b84a27d114b177b56c895a6',1,'VRegion']]],
+  ['option_8',['option',['../classVImage.html#af432280245e5c33bd800aa4a9983ea22',1,'VImage']]],
+  ['ostream_5fprint_9',['ostream_print',['../classVError.html#a8b2824da270e4d493b24fa48097165b5',1,'VError']]]
 ];
