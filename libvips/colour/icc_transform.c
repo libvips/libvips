libvips/libvips

commit 1b88a0029fac73906fee5d13aaf31c7b12138bbf

Add oklab (#4766)
* add plumbing for oklab colourspace

* add implementations

and docs

* add to tests

* add vips_image_get_interpretation_bands()

A generic thing to predict the number of bands from the interpretation.
It reduces the boilerplate when adding new colourspaces.

* add Oklch as well

* revise colour docs for new spaces

* tiny doc fix

* tweaks

polish, all tests pass cleanly

* fix normalisation

looks like the M1 and M2 matrixes already have D65 baked in

* oops forgot the *100

libvips/colour/icc_transform.c
@@ -554,43 +554,16 @@ vips_icc_print_profile(const char *name, cmsHPROFILE profile)
 static gboolean
 vips_image_is_profile_compatible(VipsImage *image, int profile_bands)
 {
-	switch (image->Type) {
-	case VIPS_INTERPRETATION_B_W:
-	case VIPS_INTERPRETATION_GREY16:
-		/* The ICC profile needs to be monochrome.
-		 */
-		return profile_bands == 1;
+	int bands = vips_interpretation_bands(image->Type);
 
-	case VIPS_INTERPRETATION_XYZ:
-	case VIPS_INTERPRETATION_LAB:
-	case VIPS_INTERPRETATION_LABQ:
-	case VIPS_INTERPRETATION_RGB:
-	case VIPS_INTERPRETATION_CMC:
-	case VIPS_INTERPRETATION_LCH:
-	case VIPS_INTERPRETATION_LABS:
-	case VIPS_INTERPRETATION_sRGB:
-	case VIPS_INTERPRETATION_YXY:
-	case VIPS_INTERPRETATION_RGB16:
-	case VIPS_INTERPRETATION_scRGB:
-	case VIPS_INTERPRETATION_HSV:
-		/* The band count in the ICC profile must correspond to that of
-		 * the image, with a maximum of 3 bands allowed.
-		 */
-		return VIPS_MIN(3, image->Bands) == profile_bands;
+	// CMYK can mean more than 4 bands for eg. hexachrome
+	if (image->Type == VIPS_INTERPRETATION_CMYK)
+		return image->Bands >= 4;
 
-	case VIPS_INTERPRETATION_CMYK:
-		/* CMYK images can only be imported if the ICC-profile has at
-		 * least 4 bands thereby blocking the usage of RGB profiles.
-		 */
-		return profile_bands >= 4;
-
-	case VIPS_INTERPRETATION_MULTIBAND:
-	case VIPS_INTERPRETATION_HISTOGRAM:
-	case VIPS_INTERPRETATION_MATRIX:
-	case VIPS_INTERPRETATION_FOURIER:
-	default:
+	if (bands > 0)
+		return bands == profile_bands;
+	else
 		return image->Bands >= profile_bands;
-	}
 }
 
 /* Load a profile from a blob and check compatibility with image, intent and

libvips/colour/Oklab2Oklch.c
@@ -1,0 +1,111 @@
+/* Turn Oklab to Oklch
+ *
+ * 3/12/25
+ * 	- from Lab2LCh.c
+ */
+
+/*
+
+	This file is part of VIPS.
+
+	VIPS is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+	02110-1301  USA
+
+ */
+
+/*
+
+	These files are distributed with VIPS - http://www.vips.ecs.soton.ac.uk
+
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /*HAVE_CONFIG_H*/
+#include <glib/gi18n-lib.h>
+
+#include <stdio.h>
+#include <math.h>
+
+#include <vips/vips.h>
+
+#include "pcolour.h"
+
+typedef VipsColourTransform VipsOklab2Oklch;
+typedef VipsColourTransformClass VipsOklab2OklchClass;
+
+G_DEFINE_TYPE(VipsOklab2Oklch, vips_Oklab2Oklch, VIPS_TYPE_COLOUR_TRANSFORM);
+
+static void
+vips_Oklab2Oklch_line(VipsColour *colour, VipsPel *out, VipsPel **in, int width)
+{
+	float *restrict p = (float *) in[0];
+	float *restrict q = (float *) out;
+
+	for (int x = 0; x < width; x++) {
+		float L = p[0];
+		float a = p[1];
+		float b = p[2];
+		p += 3;
+
+		q[0] = L;
+		q[1] = sqrtf(a * a + b * b);
+		q[2] = vips_col_ab2h(a, b);
+		q += 3;
+	}
+}
+
+static void
+vips_Oklab2Oklch_class_init(VipsOklab2OklchClass *class)
+{
+	VipsObjectClass *object_class = (VipsObjectClass *) class;
+	VipsColourClass *colour_class = VIPS_COLOUR_CLASS(class);
+
+	object_class->nickname = "Oklab2Oklch";
+	object_class->description = _("transform Oklab to Oklch");
+
+	colour_class->process_line = vips_Oklab2Oklch_line;
+}
+
+static void
+vips_Oklab2Oklch_init(VipsOklab2Oklch *Oklab2Oklch)
+{
+	VipsColour *colour = VIPS_COLOUR(Oklab2Oklch);
+
+	colour->interpretation = VIPS_INTERPRETATION_OKLCH;
+}
+
+/**
+ * vips_Oklab2Oklch: (method)
+ * @in: input image
+ * @out: (out): output image
+ * @...: `NULL`-terminated list of optional named arguments
+ *
+ * Turn Oklab to Oklch.
+ *
+ * Returns: 0 on success, -1 on error
+ */
+int
+vips_Oklab2Oklch(VipsImage *in, VipsImage **out, ...)
+{
+	va_list ap;
+	int result;
+
+	va_start(ap, out);
+	result = vips_call_split("Oklab2Oklch", ap, in, out);
+	va_end(ap);
+
+	return result;
+}

libvips/colour/Oklab2XYZ.c
@@ -1,0 +1,131 @@
+/* Oklab to XYZ.
+ *
+ */
+
+/*
+
+	This file is part of VIPS.
+
+	VIPS is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+	02110-1301  USA
+
+ */
+
+/*
+
+	These files are distributed with VIPS - http://www.vips.ecs.soton.ac.uk
+
+ */
+
+/*
+#define VIPS_DEBUG
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /*HAVE_CONFIG_H*/
+#include <glib/gi18n-lib.h>
+
+#include <stdio.h>
+#include <math.h>
+
+#include <vips/vips.h>
+#include <vips/debug.h>
+
+#include "pcolour.h"
+
+typedef VipsColourTransform VipsOklab2XYZ;
+typedef VipsColourTransformClass VipsOklab2XYZClass;
+
+G_DEFINE_TYPE(VipsOklab2XYZ, vips_Oklab2XYZ, VIPS_TYPE_COLOUR_TRANSFORM);
+
+/* Process a buffer of data.
+ */
+static void
+vips_Oklab2XYZ_line(VipsColour *colour, VipsPel *out, VipsPel **in, int width)
+{
+	float *restrict p = (float *) in[0];
+	float *restrict q = (float *) out;
+
+	for (int x = 0; x < width; x++) {
+		const float L = p[0];
+		const float a = p[1];
+		const float b = p[2];
+		p += 3;
+
+		// M2 inv to get LMS prime
+		const float lp = L * 1.         + a *  0.39633779 + b *  0.21580376;
+		const float mp = L * 1.00000001 + a * -0.10556134 + b * -0.06385417;
+		const float sp = L * 1.00000005 + a * -0.08948418 + b * -1.29148554;
+
+		// back to lms
+		const float l = lp * lp * lp;
+		const float m = mp * mp * mp;
+		const float s = sp * sp * sp;
+
+		// M1 inv to get D65 normalised XYZ
+		float X = l *  1.22701385 + m * -0.55779998 + s *  0.28125615;
+		float Y = l * -0.04058018 + m *  1.11225687 + s * -0.07167668;
+		float Z = l * -0.07638128 + m * -0.42148198 + s *  1.58616322;
+
+		q[0] = X * 100.0;
+		q[1] = Y * 100.0;
+		q[2] = Z * 100.0;
+		q += 3;
+	}
+}
+
+static void
+vips_Oklab2XYZ_class_init(VipsOklab2XYZClass *class)
+{
+	VipsObjectClass *object_class = (VipsObjectClass *) class;
+	VipsColourClass *colour_class = VIPS_COLOUR_CLASS(class);
+
+	object_class->nickname = "Oklab2XYZ";
+	object_class->description = _("transform Oklab to XYZ");
+
+	colour_class->process_line = vips_Oklab2XYZ_line;
+}
+
+static void
+vips_Oklab2XYZ_init(VipsOklab2XYZ *Oklab2XYZ)
+{
+	VipsColour *colour = VIPS_COLOUR(Oklab2XYZ);
+
+	colour->interpretation = VIPS_INTERPRETATION_XYZ;
+}
+
+/**
+ * vips_Oklab2XYZ: (method)
+ * @in: input image
+ * @out: (out): output image
+ * @...: `NULL`-terminated list of optional named arguments
+ *
+ * Transform Oklab to XYZ using D65 illuminant.
+ *
+ * Returns: 0 on success, -1 on error
+ */
+int
+vips_Oklab2XYZ(VipsImage *in, VipsImage **out, ...)
+{
+	va_list ap;
+	int result;
+
+	va_start(ap, out);
+	result = vips_call_split("Oklab2XYZ", ap, in, out);
+	va_end(ap);
+
+	return result;
+}

libvips/colour/Oklch2Oklab.c
@@ -1,0 +1,116 @@
+/* _Oklch2Oklab
+ *
+ * 3/12/25
+ * 	- from LCh2Lab.c
+ */
+
+/*
+
+	This file is part of VIPS.
+
+	VIPS is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+	02110-1301  USA
+
+ */
+
+/*
+
+	These files are distributed with VIPS - http://www.vips.ecs.soton.ac.uk
+
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /*HAVE_CONFIG_H*/
+#include <glib/gi18n-lib.h>
+
+#include <stdio.h>
+#include <math.h>
+
+#include <vips/vips.h>
+
+#include "pcolour.h"
+
+typedef VipsColourTransform VipsOklch2Oklab;
+typedef VipsColourTransformClass VipsOklch2OklabClass;
+
+G_DEFINE_TYPE(VipsOklch2Oklab, vips_Oklch2Oklab, VIPS_TYPE_COLOUR_TRANSFORM);
+
+/* Process a buffer of data.
+ */
+static void
+vips_Oklch2Oklab_line(VipsColour *colour, VipsPel *out, VipsPel **in, int width)
+{
+	float *restrict p = (float *) in[0];
+	float *restrict q = (float *) out;
+
+	for (int x = 0; x < width; x++) {
+		float L = p[0];
+		float C = p[1];
+		float h = p[2];
+		p += 3;
+
+		float a, b;
+		vips_col_Ch2ab(C, h, &a, &b);
+
+		q[0] = L;
+		q[1] = a;
+		q[2] = b;
+		q += 3;
+	}
+}
+
+static void
+vips_Oklch2Oklab_class_init(VipsOklch2OklabClass *class)
+{
+	VipsObjectClass *object_class = (VipsObjectClass *) class;
+	VipsColourClass *colour_class = VIPS_COLOUR_CLASS(class);
+
+	object_class->nickname = "Oklch2Oklab";
+	object_class->description = _("transform Oklch to Oklab");
+
+	colour_class->process_line = vips_Oklch2Oklab_line;
+}
+
+static void
+vips_Oklch2Oklab_init(VipsOklch2Oklab *Oklch2Oklab)
+{
+	VipsColour *colour = VIPS_COLOUR(Oklch2Oklab);
+
+	colour->interpretation = VIPS_INTERPRETATION_OKLAB;
+}
+
+/**
+ * vips_Oklch2Oklab: (method)
+ * @in: input image
+ * @out: (out): output image
+ * @...: `NULL`-terminated list of optional named arguments
+ *
+ * Turn Oklch to Oklab.
+ *
+ * Returns: 0 on success, -1 on error
+ */
+int
+vips_Oklch2Oklab(VipsImage *in, VipsImage **out, ...)
+{
+	va_list ap;
+	int result;
+
+	va_start(ap, out);
+	result = vips_call_split("Oklch2Oklab", ap, in, out);
+	va_end(ap);
+
+	return result;
+}

libvips/colour/XYZ2Oklab.c
@@ -1,0 +1,124 @@
+/* Transform XYZ to Oklab coordinates
+ *
+ * 2/12/25
+ *	- from XYZ2scRGB.c
+ */
+
+/*
+
+	This file is part of VIPS.
+
+	VIPS is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+	02110-1301  USA
+
+ */
+
+/*
+
+	These files are distributed with VIPS - http://www.vips.ecs.soton.ac.uk
+
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /*HAVE_CONFIG_H*/
+#include <glib/gi18n-lib.h>
+
+#include <stdio.h>
+#include <math.h>
+
+#include <vips/vips.h>
+
+#include "pcolour.h"
+
+typedef VipsColourTransform VipsXYZ2Oklab;
+typedef VipsColourTransformClass VipsXYZ2OklabClass;
+
+G_DEFINE_TYPE(VipsXYZ2Oklab, vips_XYZ2Oklab, VIPS_TYPE_COLOUR_TRANSFORM);
+
+// see https://en.wikipedia.org/wiki/Oklab_color_space#Conversion_from_CIE_XYZ
+static void
+vips_XYZ2Oklab_line(VipsColour *colour, VipsPel *out, VipsPel **in, int width)
+{
+	float *restrict p = (float *) in[0];
+	float *restrict q = (float *) out;
+
+	for (int i = 0; i < width; i++) {
+		// to D65 normalised XYZ ... M1 already has D65_X0 included etc.
+		const float X = p[0] / 100.0;
+		const float Y = p[1] / 100.0;
+		const float Z = p[2] / 100.0;
+		p += 3;
+
+		// convert to LMS
+		const float l = X * 0.8189330101 + Y * 0.3618667424 + Z * -0.1288597137;
+		const float m = X * 0.0329845436 + Y * 0.9293118715 + Z *  0.0361456387;
+		const float s = X * 0.0482003018 + Y * 0.2643662691 + Z *  0.6338517070;
+
+		// cube root ... possibly LUT this?
+		const float lp = cbrtf(l);
+		const float mp = cbrtf(m);
+		const float sp = cbrtf(s);
+
+		// to Oklab
+		q[0] = lp * 0.2104542553 + mp *  0.7936177850 + sp * -0.0040720468;
+		q[1] = lp * 1.9779984951 + mp * -2.4285922050 + sp *  0.4505937099;
+		q[2] = lp * 0.0259040371 + mp *  0.7827717662 + sp * -0.8086757660;
+		q += 3;
+	}
+}
+
+static void
+vips_XYZ2Oklab_class_init(VipsXYZ2OklabClass *class)
+{
+	VipsObjectClass *object_class = (VipsObjectClass *) class;
+	VipsColourClass *colour_class = VIPS_COLOUR_CLASS(class);
+
+	object_class->nickname = "XYZ2Oklab";
+	object_class->description = _("transform XYZ to Oklab");
+
+	colour_class->process_line = vips_XYZ2Oklab_line;
+}
+
+static void
+vips_XYZ2Oklab_init(VipsXYZ2Oklab *XYZ2Oklab)
+{
+	VipsColour *colour = VIPS_COLOUR(XYZ2Oklab);
+
+	colour->interpretation = VIPS_INTERPRETATION_OKLAB;
+}
+
+/**
+ * vips_XYZ2Oklab: (method)
+ * @in: input image
+ * @out: (out): output image
+ * @...: `NULL`-terminated list of optional named arguments
+ *
+ * Transform XYZ to Oklab assuming D65 illuminant.
+ *
+ * Returns: 0 on success, -1 on error
+ */
+int
+vips_XYZ2Oklab(VipsImage *in, VipsImage **out, ...)
+{
+	va_list ap;
+	int result;
+
+	va_start(ap, out);
+	result = vips_call_split("XYZ2Oklab", ap, in, out);
+	va_end(ap);
+
+	return result;
+}

libvips/colour/colour.c
@@ -638,6 +638,10 @@ vips_colour_operation_init(void)
 	extern GType vips_scRGB2sRGB_get_type(void);
 	extern GType vips_CMYK2XYZ_get_type(void);
 	extern GType vips_XYZ2CMYK_get_type(void);
+	extern GType vips_XYZ2Oklab_get_type(void);
+	extern GType vips_Oklab2XYZ_get_type(void);
+	extern GType vips_Oklab2Oklch_get_type(void);
+	extern GType vips_Oklch2Oklab_get_type(void);
 	extern GType vips_uhdr2scRGB_get_type(void);
 	extern GType vips_profile_load_get_type(void);
 #ifdef HAVE_LCMS2
@@ -650,6 +654,10 @@ vips_colour_operation_init(void)
 	extern GType vips_dECMC_get_type(void);
 
 	vips_colourspace_get_type();
+	vips_Oklab2Oklch_get_type();
+	vips_Oklch2Oklab_get_type();
+	vips_Oklab2XYZ_get_type();
+	vips_XYZ2Oklab_get_type();
 	vips_Lab2XYZ_get_type();
 	vips_XYZ2Lab_get_type();
 	vips_Lab2LCh_get_type();

libvips/colour/colourspace.c
@@ -215,6 +215,8 @@ typedef struct _VipsColourRoute {
 #define RGB16 VIPS_INTERPRETATION_RGB16
 #define GREY16 VIPS_INTERPRETATION_GREY16
 #define YXY VIPS_INTERPRETATION_YXY
+#define OKLAB VIPS_INTERPRETATION_OKLAB
+#define OKLCH VIPS_INTERPRETATION_OKLCH
 
 /* All the routes we know about.
  */
@@ -233,6 +235,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ XYZ, RGB16, { vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ XYZ, GREY16, { vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ XYZ, YXY, { vips_XYZ2Yxy, NULL } },
+	{ XYZ, OKLAB, { vips_XYZ2Oklab, NULL } },
+	{ XYZ, OKLCH, { vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ LAB, XYZ, { vips_Lab2XYZ, NULL } },
 	{ LAB, LAB, { vips_cast_float, NULL } },
@@ -248,6 +252,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ LAB, RGB16, { vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ LAB, GREY16, { vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ LAB, YXY, { vips_Lab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ LAB, OKLAB, { vips_Lab2XYZ, vips_XYZ2Oklab, NULL } },
+	{ LAB, OKLCH, { vips_Lab2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ LABQ, XYZ, { vips_LabQ2Lab, vips_Lab2XYZ, NULL } },
 	{ LABQ, LAB, { vips_LabQ2Lab, NULL } },
@@ -263,6 +269,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ LABQ, RGB16, { vips_LabQ2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ LABQ, GREY16, { vips_LabQ2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ LABQ, YXY, { vips_LabQ2Lab, vips_Lab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ LABQ, OKLAB, { vips_LabQ2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, NULL } },
+	{ LABQ, OKLCH, { vips_LabQ2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ LCH, XYZ, { vips_LCh2Lab, vips_Lab2XYZ, NULL } },
 	{ LCH, LAB, { vips_LCh2Lab, NULL } },
@@ -278,6 +286,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ LCH, RGB16, { vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ LCH, GREY16, { vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ LCH, YXY, { vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ LCH, OKLAB, { vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, NULL } },
+	{ LCH, OKLCH, { vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ CMC, XYZ, { vips_CMC2LCh, vips_LCh2Lab, vips_Lab2XYZ, NULL } },
 	{ CMC, LAB, { vips_CMC2LCh, vips_LCh2Lab, NULL } },
@@ -293,6 +303,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ CMC, RGB16, { vips_CMC2LCh, vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ CMC, GREY16, { vips_CMC2LCh, vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ CMC, YXY, { vips_CMC2LCh, vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ CMC, OKLAB, { vips_CMC2LCh, vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, NULL } },
+	{ CMC, OKLCH, { vips_CMC2LCh, vips_LCh2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ LABS, XYZ, { vips_LabS2Lab, vips_Lab2XYZ, NULL } },
 	{ LABS, LAB, { vips_LabS2Lab, NULL } },
@@ -308,6 +320,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ LABS, RGB16, { vips_LabS2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ LABS, GREY16, { vips_LabS2Lab, vips_Lab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ LABS, YXY, { vips_LabS2Lab, vips_Lab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ LABS, OKLAB, { vips_LabS2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, NULL } },
+	{ LABS, OKLCH, { vips_LabS2Lab, vips_Lab2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ CMYK, XYZ, { vips_CMYK2XYZ, NULL } },
 	{ CMYK, LAB, { vips_CMYK2XYZ, vips_XYZ2Lab, NULL } },
@@ -324,6 +338,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ CMYK, RGB16, { vips_CMYK2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ CMYK, GREY16, { vips_CMYK2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ CMYK, YXY, { vips_CMYK2XYZ, vips_XYZ2Yxy, NULL } },
+	{ CMYK, OKLAB, { vips_CMYK2XYZ, vips_XYZ2Oklab, NULL } },
+	{ CMYK, OKLCH, { vips_CMYK2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ scRGB, XYZ, { vips_scRGB2XYZ, NULL } },
 	{ scRGB, LAB, { vips_scRGB2XYZ, vips_XYZ2Lab, NULL } },
@@ -339,6 +355,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ scRGB, RGB16, { vips_scRGB2RGB16, NULL } },
 	{ scRGB, GREY16, { vips_scRGB2BW16, NULL } },
 	{ scRGB, YXY, { vips_scRGB2XYZ, vips_XYZ2Yxy, NULL } },
+	{ scRGB, OKLAB, { vips_scRGB2XYZ, vips_XYZ2Oklab, NULL } },
+	{ scRGB, OKLCH, { vips_scRGB2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ sRGB, XYZ, { vips_sRGB2scRGB, vips_scRGB2XYZ, NULL } },
 	{ sRGB, LAB, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Lab, NULL } },
@@ -354,6 +372,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ sRGB, RGB16, { vips_sRGB2RGB16, NULL } },
 	{ sRGB, GREY16, { vips_sRGB2scRGB, vips_scRGB2BW16, NULL } },
 	{ sRGB, YXY, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Yxy, NULL } },
+	{ sRGB, OKLAB, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, NULL } },
+	{ sRGB, OKLCH, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ HSV, XYZ, { vips_HSV2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, NULL } },
 	{ HSV, LAB, { vips_HSV2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Lab, NULL } },
@@ -369,6 +389,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ HSV, RGB16, { vips_HSV2sRGB, vips_sRGB2RGB16, NULL } },
 	{ HSV, GREY16, { vips_HSV2sRGB, vips_sRGB2scRGB, vips_scRGB2BW16, NULL } },
 	{ HSV, YXY, { vips_HSV2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Yxy, NULL } },
+	{ HSV, OKLAB, { vips_HSV2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, NULL } },
+	{ HSV, OKLCH, { vips_HSV2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ BW, XYZ, { vips_BW2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, NULL } },
 	{ BW, LAB, { vips_BW2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Lab, NULL } },
@@ -384,6 +406,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ BW, RGB16, { vips_BW2sRGB, vips_sRGB2RGB16, NULL } },
 	{ BW, GREY16, { vips_BW2sRGB, vips_sRGB2scRGB, vips_scRGB2BW16, NULL } },
 	{ BW, YXY, { vips_BW2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Yxy, NULL } },
+	{ BW, OKLAB, { vips_BW2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, NULL } },
+	{ BW, OKLCH, { vips_BW2sRGB, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ RGB16, XYZ, { vips_sRGB2scRGB, vips_scRGB2XYZ, NULL } },
 	{ RGB16, LAB, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Lab, NULL } },
@@ -399,6 +423,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ RGB16, RGB16, { vips_cast_ushort, NULL } },
 	{ RGB16, GREY16, { vips_sRGB2scRGB, vips_scRGB2BW16, NULL } },
 	{ RGB16, YXY, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Yxy, NULL } },
+	{ RGB16, OKLAB, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, NULL } },
+	{ RGB16, OKLCH, { vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ GREY16, XYZ, { vips_GREY162RGB16, vips_sRGB2scRGB, vips_scRGB2XYZ, NULL } },
 	{ GREY16, LAB, { vips_GREY162RGB16, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Lab, NULL } },
@@ -414,6 +440,8 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ GREY16, RGB16, { vips_GREY162RGB16, NULL } },
 	{ GREY16, GREY16, { vips_cast_ushort, NULL } },
 	{ GREY16, YXY, { vips_GREY162RGB16, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Yxy, NULL } },
+	{ GREY16, OKLAB, { vips_GREY162RGB16, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, NULL } },
+	{ GREY16, OKLCH, { vips_GREY162RGB16, vips_sRGB2scRGB, vips_scRGB2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
 
 	{ YXY, XYZ, { vips_Yxy2XYZ, NULL } },
 	{ YXY, LAB, { vips_Yxy2XYZ, vips_XYZ2Lab, NULL } },
@@ -429,6 +457,42 @@ static VipsColourRoute vips_colour_routes[] = {
 	{ YXY, RGB16, { vips_Yxy2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
 	{ YXY, GREY16, { vips_Yxy2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
 	{ YXY, YXY, { vips_cast_float, NULL } },
+	{ YXY, OKLAB, { vips_Yxy2XYZ, vips_XYZ2Oklab, NULL } },
+	{ YXY, OKLCH, { vips_Yxy2XYZ, vips_XYZ2Oklab, vips_Oklab2Oklch, NULL } },
+
+	{ OKLAB, XYZ, { vips_Oklab2XYZ, NULL } },
+	{ OKLAB, LAB, { vips_Oklab2XYZ, vips_XYZ2Lab, NULL } },
+	{ OKLAB, LABQ, { vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LabQ, NULL } },
+	{ OKLAB, LCH, { vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LCh, NULL } },
+	{ OKLAB, CMC, { vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LCh, vips_LCh2CMC, NULL } },
+	{ OKLAB, LABS, { vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LabS, NULL } },
+	{ OKLAB, CMYK, { vips_Oklab2XYZ, vips_XYZ2CMYK, NULL } },
+	{ OKLAB, scRGB, { vips_Oklab2XYZ, vips_XYZ2scRGB, NULL } },
+	{ OKLAB, sRGB, { vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2sRGB, NULL } },
+	{ OKLAB, HSV, { vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2sRGB, vips_sRGB2HSV, NULL } },
+	{ OKLAB, BW, { vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW, NULL } },
+	{ OKLAB, RGB16, { vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
+	{ OKLAB, GREY16, { vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
+	{ OKLAB, YXY, { vips_Oklab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ OKLAB, OKLAB, { vips_cast_float, NULL } },
+	{ OKLAB, OKLCH, { vips_Oklab2Oklch, NULL } },
+
+	{ OKLCH, XYZ, { vips_Oklch2Oklab, vips_Oklab2XYZ, NULL } },
+	{ OKLCH, LAB, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2Lab, NULL } },
+	{ OKLCH, LABQ, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LabQ, NULL } },
+	{ OKLCH, LCH, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LCh, NULL } },
+	{ OKLCH, CMC, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LCh, vips_LCh2CMC, NULL } },
+	{ OKLCH, LABS, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2Lab, vips_Lab2LabS, NULL } },
+	{ OKLCH, CMYK, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2CMYK, NULL } },
+	{ OKLCH, scRGB, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2scRGB, NULL } },
+	{ OKLCH, sRGB, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2sRGB, NULL } },
+	{ OKLCH, HSV, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2sRGB, vips_sRGB2HSV, NULL } },
+	{ OKLCH, BW, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW, NULL } },
+	{ OKLCH, RGB16, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2RGB16, NULL } },
+	{ OKLCH, GREY16, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2scRGB, vips_scRGB2BW16, NULL } },
+	{ OKLCH, YXY, { vips_Oklch2Oklab, vips_Oklab2XYZ, vips_XYZ2Yxy, NULL } },
+	{ OKLCH, OKLAB, { vips_Oklch2Oklab, NULL } },
+	{ OKLCH, OKLCH, { vips_cast_float, NULL } },
 
 };
 

libvips/colour/meson.build
@@ -19,6 +19,9 @@ colour_sources = files(
     'LabS2LabQ.c',
     'LCh2Lab.c',
     'LCh2UCS.c',
+    'Oklab2XYZ.c',
+    'Oklab2Oklch.c',
+    'Oklch2Oklab.c',
     'profile_load.c',
     'profiles.c',
     'rad2float.c',
@@ -32,6 +35,7 @@ colour_sources = files(
     'XYZ2CMYK.c',
     'XYZ2Lab.c',
     'XYZ2scRGB.c',
+    'XYZ2Oklab.c',
     'XYZ2Yxy.c',
     'Yxy2XYZ.c',
   )

commit 955c0671e156fca00dfa2e3956d53b3c722d626b

Discard existing RGB input profile for CMYK output
Fixes a regression introduced in commit 1b88a00.

libvips/colour/icc_transform.c
@@ -558,7 +558,7 @@ vips_image_is_profile_compatible(VipsImage *image, int profile_bands)
 
 	// CMYK can mean more than 4 bands for eg. hexachrome
 	if (image->Type == VIPS_INTERPRETATION_CMYK)
-		return image->Bands >= 4;
+		return profile_bands >= 4;
 
 	if (bands > 0)
 		return bands == profile_bands;