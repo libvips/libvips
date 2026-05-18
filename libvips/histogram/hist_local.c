libvips/libvips

commit 9bde113b9dd0eeae39a9c9d5b92a957490586dc7

stdif: support many band images (#5016)
Also re-sync this operation and hist_local with rank.

libvips/histogram/hist_local.c
@@ -102,13 +102,11 @@ vips_hist_local_stop(void *vseq, void *a, void *b)
 	VipsImage *in = (VipsImage *) a;
 
 	VIPS_UNREF(seq->ir);
-	if (seq->hist &&
-		in) {
-		int i;
 
-		for (i = 0; i < in->Bands; i++)
+	if (seq->hist &&
+		in)
+		for (int i = 0; i < in->Bands; i++)
 			VIPS_FREE(seq->hist[i]);
-	}
 	VIPS_FREE(seq->hist);
 	VIPS_FREE(seq);
 
@@ -121,8 +119,6 @@ vips_hist_local_start(VipsImage *out, void *a, void *b)
 	VipsImage *in = (VipsImage *) a;
 	VipsHistLocalSequence *seq;
 
-	int i;
-
 	if (!(seq = VIPS_NEW(NULL, VipsHistLocalSequence)))
 		return NULL;
 	seq->ir = NULL;
@@ -134,7 +130,7 @@ vips_hist_local_start(VipsImage *out, void *a, void *b)
 		return NULL;
 	}
 
-	for (i = 0; i < in->Bands; i++)
+	for (int i = 0; i < in->Bands; i++)
 		if (!(seq->hist[i] = VIPS_ARRAY(NULL, 256, unsigned int))) {
 			vips_hist_local_stop(seq, NULL, NULL);
 			return NULL;
@@ -154,24 +150,23 @@ vips_hist_local_generate(VipsRegion *out_region,
 	const int bands = in->Bands;
 	const int max_slope = local->max_slope;
 
-	VipsRect irect;
-	int y;
+	VipsRect s;
 	int lsk;
 	int centre; /* Offset to move to centre of window */
 
-	/* What part of ir do we need?
+	/* Prepare the section of the input image we need. A little larger
+	 * than the section of the output image we are producing.
 	 */
-	irect.left = r->left;
-	irect.top = r->top;
-	irect.width = r->width + local->width;
-	irect.height = r->height + local->height;
-	if (vips_region_prepare(seq->ir, &irect))
+	s = *r;
+	s.width += local->width - 1;
+	s.height += local->height - 1;
+	if (vips_region_prepare(seq->ir, &s))
 		return -1;
 
 	lsk = VIPS_REGION_LSKIP(seq->ir);
 	centre = lsk * (local->height / 2) + bands * (local->width / 2);
 
-	for (y = 0; y < r->height; y++) {
+	for (int y = 0; y < r->height; y++) {
 		/* Get input and output pointers for this line.
 		 */
 		VipsPel *restrict p =
@@ -180,16 +175,18 @@ vips_hist_local_generate(VipsRegion *out_region,
 			VIPS_REGION_ADDR(out_region, r->left, r->top + y);
 
 		VipsPel *restrict p1;
-		int x, i, j, b;
 
 		/* Find histogram for the start of this line.
 		 */
-		for (b = 0; b < bands; b++)
+		for (int b = 0; b < bands; b++)
 			memset(seq->hist[b], 0, 256 * sizeof(unsigned int));
 		p1 = p;
-		for (j = 0; j < local->height; j++) {
-			for (i = 0, x = 0; x < local->width; x++)
-				for (b = 0; b < bands; b++, i++)
+		for (int j = 0; j < local->height; j++) {
+			int i;
+
+			i = 0;
+			for (int x = 0; x < local->width; x++)
+				for (int b = 0; b < bands; b++, i++)
 					seq->hist[b][p1[i]] += 1;
 
 			p1 += lsk;
@@ -197,8 +194,8 @@ vips_hist_local_generate(VipsRegion *out_region,
 
 		/* Loop for output pels.
 		 */
-		for (x = 0; x < r->width; x++) {
-			for (b = 0; b < bands; b++) {
+		for (int x = 0; x < r->width; x++) {
+			for (int b = 0; b < bands; b++) {
 				/* Sum histogram up to current pel.
 				 */
 				unsigned int *restrict hist = seq->hist[b];
@@ -208,22 +205,21 @@ vips_hist_local_generate(VipsRegion *out_region,
 
 				sum = 0;
 
-				/* For CLAHE we need to limit the height of the
-				 * hist to limit the amount we boost the
-				 * contrast by.
+				/* For CLAHE we need to limit the height of the hist to limit
+				 * the amount we boost the contrast by.
 				 */
 				if (max_slope > 0) {
+					int i;
 					int sum_over;
 
 					sum_over = 0;
 
-					/* Must be <= target, since a cum hist
-					 * always includes the current element.
+					/* Must be <= target, since a cum hist always includes
+					 * the current element.
 					 */
 					for (i = 0; i <= target; i++) {
 						if (hist[i] > max_slope) {
-							sum_over += hist[i] -
-								max_slope;
+							sum_over += hist[i] - max_slope;
 							sum += max_slope;
 						}
 						else
@@ -232,39 +228,34 @@ vips_hist_local_generate(VipsRegion *out_region,
 
 					for (; i < 256; i++) {
 						if (hist[i] > max_slope)
-							sum_over += hist[i] -
-								max_slope;
+							sum_over += hist[i] - max_slope;
 					}
 
-					/* The extra clipped off bit from the
-					 * top of the hist is spread over all
-					 * bins equally, then summed to target.
+					/* The extra clipped off bit from the top of the hist is
+					 * spread over all bins equally, then summed to target.
 					 */
 					sum += (target + 1) * sum_over / 256;
 				}
 				else {
 					sum = 0;
-					for (i = 0; i <= target; i++)
+					for (int i = 0; i <= target; i++)
 						sum += hist[i];
 				}
 
-				/* This can't overflow, even in
-				 * contrast-limited mode.
+				/* This can't overflow, even in contrast-limited mode.
 				 *
-				 * Scale by 255, not 256, or we'll get
-				 * overflow.
+				 * Scale by 255, not 256, or we'll get overflow.
 				 */
-				q[b] = 255 * sum /
-					(local->width * local->height);
+				q[b] = 255 * sum / (local->width * local->height);
 
-				/* Adapt histogram -- remove the pels from
-				 * the left hand column, add in pels for a
-				 * new right-hand column.
+				/* Adapt histogram -- remove the pels from the left hand column,
+				 * add in pels for a new right-hand column.
 				 */
+				const int next = bands * local->width;
 				p1 = p + b;
-				for (j = 0; j < local->height; j++) {
+				for (int j = 0; j < local->height; j++) {
 					hist[p1[0]] -= 1;
-					hist[p1[bands * local->width]] += 1;
+					hist[p1[next]] += 1;
 
 					p1 += lsk;
 				}

libvips/histogram/stdif.c
@@ -86,72 +86,113 @@ typedef VipsOperationClass VipsStdifClass;
 
 G_DEFINE_TYPE(VipsStdif, vips_stdif, VIPS_TYPE_OPERATION);
 
-/* How ugly and stupid.
+/* Our sequence value.
  */
-#define MAX_BANDS (100)
+typedef struct {
+	VipsRegion *ir; /* Input region */
+
+	/* The sums for each band.
+	 */
+	unsigned int *sum;
+	unsigned int *sum2;
+} VipsStdifSequence;
+
+static int
+vips_stdif_stop(void *vseq, void *a, void *b)
+{
+	VipsStdifSequence *seq = (VipsStdifSequence *) vseq;
+
+	VIPS_UNREF(seq->ir);
+	VIPS_FREE(seq->sum);
+	VIPS_FREE(seq->sum2);
+	VIPS_FREE(seq);
+
+	return 0;
+}
+
+static void *
+vips_stdif_start(VipsImage *out, void *a, void *b)
+{
+	VipsImage *in = (VipsImage *) a;
+	VipsStdifSequence *seq;
+
+	if (!(seq = VIPS_NEW(NULL, VipsStdifSequence)))
+		return NULL;
+	seq->ir = NULL;
+	seq->sum = NULL;
+	seq->sum2 = NULL;
+
+	if (!(seq->ir = vips_region_new(in)) ||
+		!(seq->sum = VIPS_ARRAY(NULL, in->Bands, unsigned int)) ||
+		!(seq->sum2 = VIPS_ARRAY(NULL, in->Bands, unsigned int))) {
+		vips_stdif_stop(seq, NULL, NULL);
+		return NULL;
+	}
+
+	return seq;
+}
 
 static int
 vips_stdif_generate(VipsRegion *out_region,
 	void *vseq, void *a, void *b, gboolean *stop)
 {
-	VipsRect *r = &out_region->valid;
-	VipsRegion *ir = (VipsRegion *) vseq;
+	VipsStdifSequence *seq = (VipsStdifSequence *) vseq;
 	VipsImage *in = (VipsImage *) a;
 	VipsStdif *stdif = (VipsStdif *) b;
-	int bands = in->Bands;
-	int npel = stdif->width * stdif->height;
+	VipsRect *r = &out_region->valid;
+	const int bands = in->Bands;
+	const int npel = stdif->width * stdif->height;
 
-	VipsRect irect;
-	int y;
+	VipsRect s;
 	int lsk;
 	int centre; /* Offset to move to centre of window */
 
-	/* What part of ir do we need?
+	/* Prepare the section of the input image we need. A little larger
+	 * than the section of the output image we are producing.
 	 */
-	irect.left = out_region->valid.left;
-	irect.top = out_region->valid.top;
-	irect.width = out_region->valid.width + stdif->width;
-	irect.height = out_region->valid.height + stdif->height;
-	if (vips_region_prepare(ir, &irect))
+	s = *r;
+	s.width += stdif->width - 1;
+	s.height += stdif->height - 1;
+	if (vips_region_prepare(seq->ir, &s))
 		return -1;
 
-	lsk = VIPS_REGION_LSKIP(ir);
-	centre = lsk * (stdif->height / 2) + stdif->width / 2;
+	lsk = VIPS_REGION_LSKIP(seq->ir);
+	centre = lsk * (stdif->height / 2) + bands * (stdif->width / 2);
 
-	for (y = 0; y < r->height; y++) {
+	for (int y = 0; y < r->height; y++) {
 		/* Get input and output pointers for this line.
 		 */
-		VipsPel *p = VIPS_REGION_ADDR(ir, r->left, r->top + y);
-		VipsPel *q = VIPS_REGION_ADDR(out_region, r->left, r->top + y);
+		VipsPel *restrict p =
+			VIPS_REGION_ADDR(seq->ir, r->left, r->top + y);
+		VipsPel *restrict q =
+			VIPS_REGION_ADDR(out_region, r->left, r->top + y);
 
 		double f1 = stdif->a * stdif->m0;
 		double f2 = 1.0 - stdif->a;
 		double f3 = stdif->b * stdif->s0;
 
-		VipsPel *p1;
-		int x, i, j, b;
+		VipsPel *restrict p1;
 
 		/* We will get int overflow for windows larger than about 256
 		 * x 256, sadly.
 		 */
-		unsigned int sum[MAX_BANDS];
-		unsigned int sum2[MAX_BANDS];
 
 		/* Find sum, sum of squares for the start of this line.
 		 */
-		for (b = 0; b < bands; b++) {
-			memset(sum, 0, bands * sizeof(unsigned int));
-			memset(sum2, 0, bands * sizeof(unsigned int));
-		}
-		p1 = p;
-		for (j = 0; j < stdif->height; j++) {
-			i = 0;
-			for (x = 0; x < stdif->width; x++) {
-				for (b = 0; b < bands; b++) {
-					int t = p1[i++];
+		memset(seq->sum, 0, bands * sizeof(unsigned int));
+		memset(seq->sum2, 0, bands * sizeof(unsigned int));
 
-					sum[b] += t;
-					sum2[b] += t * t;
+		p1 = p;
+		for (int j = 0; j < stdif->height; j++) {
+			int i;
+
+			i = 0;
+			for (int x = 0; x < stdif->width; x++) {
+				for (int b = 0; b < bands; b++, i++) {
+					int t = p1[i];
+
+					seq->sum[b] += t;
+					seq->sum2[b] += t * t;
 				}
 			}
 
@@ -160,50 +201,51 @@ vips_stdif_generate(VipsRegion *out_region,
 
 		/* Loop for output pels.
 		 */
-		for (x = 0; x < r->width; x++) {
-			for (b = 0; b < bands; b++) {
+		for (int x = 0; x < r->width; x++) {
+			for (int b = 0; b < bands; b++) {
 				/* Find stats.
 				 */
-				double mean = (double) sum[b] / npel;
-				double var = (double) sum2[b] / npel -
+				double mean = (double) seq->sum[b] / npel;
+				double var = (double) seq->sum2[b] / npel -
 					(mean * mean);
 				double sig = sqrt(var);
 
 				/* Transform.
 				 */
 				double res = f1 + f2 * mean +
-					((double) p[centre] - mean) *
+					((double) p[centre + b] - mean) *
 						(f3 / (stdif->s0 + stdif->b * sig));
 
 				/* And write.
 				 */
 				if (res < 0.0)
-					*q++ = 0;
+					q[b] = 0;
 				else if (res >= 256.0)
-					*q++ = 255;
+					q[b] = 255;
 				else
-					*q++ = res + 0.5;
+					q[b] = res + 0.5;
 
-				/* Adapt sums - remove the pels from the left
-				 * hand column, add in pels for a new
-				 * right-hand column.
+				/* Adapt sums -- remove the pels from the left hand column,
+				 * add in pels for a new right-hand column.
 				 */
-				p1 = p;
-				for (j = 0; j < stdif->height; j++) {
+				const int next = bands * stdif->width;
+				p1 = p + b;
+				for (int j = 0; j < stdif->height; j++) {
 					int t1 = p1[0];
-					int t2 = p1[bands * stdif->width];
+					int t2 = p1[next];
 
-					sum[b] -= t1;
-					sum2[b] -= t1 * t1;
+					seq->sum[b] -= t1;
+					seq->sum2[b] -= t1 * t1;
 
-					sum[b] += t2;
-					sum2[b] += t2 * t2;
+					seq->sum[b] += t2;
+					seq->sum2[b] += t2 * t2;
 
 					p1 += lsk;
 				}
-
-				p += 1;
 			}
+
+			p += bands;
+			q += bands;
 		}
 	}
 
@@ -236,10 +278,6 @@ vips_stdif_build(VipsObject *object)
 		vips_error(class->nickname, "%s", _("window too large"));
 		return -1;
 	}
-	if (in->Bands > MAX_BANDS) {
-		vips_error(class->nickname, "%s", _("too many bands"));
-		return -1;
-	}
 
 	/* Expand the input.
 	 */
@@ -263,9 +301,9 @@ vips_stdif_build(VipsObject *object)
 	stdif->out->Ysize -= stdif->height - 1;
 
 	if (vips_image_generate(stdif->out,
-			vips_start_one,
+			vips_stdif_start,
 			vips_stdif_generate,
-			vips_stop_one,
+			vips_stdif_stop,
 			in, stdif))
 		return -1;
 
@@ -282,6 +320,7 @@ vips_stdif_class_init(VipsStdifClass *class)
 {
 	GObjectClass *gobject_class = G_OBJECT_CLASS(class);
 	VipsObjectClass *object_class = (VipsObjectClass *) class;
+	VipsOperationClass *operation_class = VIPS_OPERATION_CLASS(class);
 
 	gobject_class->set_property = vips_object_set_property;
 	gobject_class->get_property = vips_object_get_property;
@@ -289,6 +328,8 @@ vips_stdif_class_init(VipsStdifClass *class)
 	object_class->nickname = "stdif";
 	object_class->description = _("statistical difference");
 	object_class->build = vips_stdif_build;
+
+	operation_class->flags = VIPS_OPERATION_SEQUENTIAL;
 
 	VIPS_ARG_IMAGE(class, "in", 1,
 		_("Input"),
@@ -388,13 +429,14 @@ vips_stdif_init(VipsStdif *stdif)
  *
  * Try:
  *
- * ```
- * vips stdif $VIPSHOME/pics/huysum.v fred.v 0.5 128 0.5 50 11 11
+ * ```bash
+ * $ vips stdif $VIPSHOME/pics/huysum.v fred.v 0.5 128 0.5 50 11 11
  * ```
  *
- * The operation works on one-band uchar images only, and writes a one-band
- * uchar image as its result. The output image has the same size as the
- * input.
+ * It works for uchar images only, with any number of bands.
+ * The input is expanded by copying edge pixels before performing the
+ * operation so that the output image has the same size as the input.
+ * Edge pixels in the output image are therefore only approximate.
  *
  * ::: tip "Optional arguments"
  *     * @a: `gdouble`, weight of new mean

commit f93ceb3354ba12dbc609184d57c576918bf64ac6

histogram: fix an off-by-one error
Fixes a regression introduced in commit 9bde113.

libvips/histogram/hist_local.c
@@ -158,7 +158,7 @@ vips_hist_local_generate(VipsRegion *out_region,
 	 * than the section of the output image we are producing.
 	 */
 	s = *r;
-	s.width += local->width - 1;
+	s.width += local->width;
 	s.height += local->height - 1;
 	if (vips_region_prepare(seq->ir, &s))
 		return -1;

libvips/histogram/stdif.c
@@ -151,7 +151,7 @@ vips_stdif_generate(VipsRegion *out_region,
 	 * than the section of the output image we are producing.
 	 */
 	s = *r;
-	s.width += stdif->width - 1;
+	s.width += stdif->width;
 	s.height += stdif->height - 1;
 	if (vips_region_prepare(seq->ir, &s))
 		return -1;