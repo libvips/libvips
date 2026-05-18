libvips/libvips

commit e96a1f299ebe34f7833bb46190d036b7da87aeb4

rank: expand input by one column (#5015)
In line with commit 9820ccf9f2c58aa822c70074567cb358ce062266.

libvips/morphology/rank.c
@@ -505,7 +505,7 @@ vips_rank_build(VipsObject *object)
 	 */
 	if (vips_embed(in, &t[1],
 			rank->width / 2, rank->height / 2,
-			in->Xsize + rank->width - 1, in->Ysize + rank->height - 1,
+			in->Xsize + rank->width, in->Ysize + rank->height - 1,
 			"extend", VIPS_EXTEND_COPY,
 			NULL))
 		return -1;
@@ -519,7 +519,7 @@ vips_rank_build(VipsObject *object)
 	if (vips_image_pipelinev(rank->out,
 			VIPS_DEMAND_STYLE_FATSTRIP, in, NULL))
 		return -1;
-	rank->out->Xsize -= rank->width - 1;
+	rank->out->Xsize -= rank->width;
 	rank->out->Ysize -= rank->height - 1;
 
 	if (vips_image_generate(rank->out,

commit aab5043f7084d419e32a05beaea82ce082283eca

rank: fix an off-by-one error
Fixes a regression introduced in commit e96a1f2.

libvips/morphology/rank.c
@@ -434,7 +434,7 @@ vips_rank_generate(VipsRegion *out_region,
 	 * than the section of the output image we are producing.
 	 */
 	s = *r;
-	s.width += rank->width - 1;
+	s.width += rank->width;
 	s.height += rank->height - 1;
 	if (vips_region_prepare(ir, &s))
 		return -1;