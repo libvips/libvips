libvips/libvips

commit 090aff066089ab6264119d89d080694df25f4941

image: avoid attaching/updating time struct to `->progress_signal` (#5097)
Do this only when building with `-Ddeprecated=true`.

libvips/iofuncs/image.c
@@ -1530,11 +1530,13 @@ vips_image_preeval(VipsImage *image)
 
 		(void) vips_progress_add(image);
 
+#ifdef ENABLE_DEPRECATED
 		/* For vips7 compat, we also have to make sure ->time on the
 		 * image that was originally marked with
 		 * vips_image_set_progress() is valid.
 		 */
 		(void) vips_progress_add(image->progress_signal);
+#endif
 
 		if (!vips_image_get_typeof(image, "hide-progress"))
 			g_signal_emit(image->progress_signal,
@@ -1556,12 +1558,14 @@ vips_image_eval(VipsImage *image, guint64 processed)
 
 		vips_progress_update(image->time, processed);
 
+#ifdef ENABLE_DEPRECATED
 		/* For vips7 compat, update the ->time on the signalling image
 		 * too, even though it may have a different width/height to
 		 * the image we are actually generating.
 		 */
 		if (image->progress_signal->time != image->time)
 			vips_progress_update(image->progress_signal->time, processed);
+#endif
 
 		if (!vips_image_get_typeof(image, "hide-progress"))
 			g_signal_emit(image->progress_signal,
@@ -1586,6 +1590,7 @@ vips_image_posteval(VipsImage *image)
 		processed = image->time->tpels;
 		vips_progress_update(image->time, processed);
 
+#ifdef ENABLE_DEPRECATED
 		/* For vips7 compat, update the ->time on the signalling image
 		 * too, even though it may have a different width/height to
 		 * the image we are actually generating.
@@ -1593,6 +1598,7 @@ vips_image_posteval(VipsImage *image)
 		if (image->progress_signal->time != image->time)
 			vips_progress_update(image->progress_signal->time,
 				processed);
+#endif
 
 		if (!vips_image_get_typeof(image, "hide-progress"))
 			g_signal_emit(image->progress_signal,

commit 20a646019266a1d33bd159ffaf103e57028e96ec

image: ensure `posteval` signal is emitted
Fixes a regression introduced in commit 090aff0.

libvips/iofuncs/image.c
@@ -1577,7 +1577,7 @@ void
 vips_image_posteval(VipsImage *image)
 {
 	if (image->progress_signal &&
-		image->progress_signal->time) {
+		image->time) {
 		gint64 processed;
 
 		VIPS_DEBUG_MSG("vips_image_posteval: %p\n", image);