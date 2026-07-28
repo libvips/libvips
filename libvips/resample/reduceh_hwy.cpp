libvips/libvips

commit 46dde2e6500a2bb0bfde860cd0ce4b5e1110075b

Fix Highway paths on big-endian targets (#4817)
* convi: fix Highway path on big-endian targets

* reduceh: fix Highway path on big-endian targets

* reducev: fix Highway path on big-endian targets

* shrinkv: fix Highway path on big-endian targets

libvips/resample/reduceh_hwy.cpp
@@ -68,6 +68,12 @@ constexpr DI8 di8;
 constexpr DI16 di16;
 constexpr DI32 di32;
 
+#if HWY_IS_BIG_ENDIAN
+#define HWY_ENDIAN_LOHI(lo, hi) hi, lo
+#else
+#define HWY_ENDIAN_LOHI(lo, hi) lo, hi
+#endif
+
 HWY_ATTR void
 vips_reduceh_uchar_hwy(VipsPel *pout, VipsPel *pin,
 	int32_t n, int32_t width, int32_t bands,
@@ -82,21 +88,29 @@ vips_reduceh_uchar_hwy(VipsPel *pout, VipsPel *pin,
 	 * r0 g0 b0 r1 g1 b1 r2 g2 b2 r3 g3 b3
 	 */
 	const auto shuf3_lo = Dup128VecFromValues(di8,
-		0, -1, 3, -1, 1, -1, 4, -1,
-		2, -1, 5, -1, -1, -1, -1, -1);
+		HWY_ENDIAN_LOHI(0, -1), HWY_ENDIAN_LOHI(3, -1),
+		HWY_ENDIAN_LOHI(1, -1), HWY_ENDIAN_LOHI(4, -1),
+		HWY_ENDIAN_LOHI(2, -1), HWY_ENDIAN_LOHI(5, -1),
+		-1, -1, -1, -1);
 	const auto shuf3_hi = Dup128VecFromValues(di8,
-		6, -1, 9, -1, 7, -1, 10, -1,
-		8, -1, 11, -1, -1, -1, -1, -1);
+		HWY_ENDIAN_LOHI(6, -1), HWY_ENDIAN_LOHI(9, -1),
+		HWY_ENDIAN_LOHI(7, -1), HWY_ENDIAN_LOHI(10, -1),
+		HWY_ENDIAN_LOHI(8, -1), HWY_ENDIAN_LOHI(11, -1),
+		-1, -1, -1, -1);
 
 	/*  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
 	 * r0 g0 b0 a0 r1 g1 b1 a1 r2 g2 b2 a2 r3 g3 b3 a3
 	 */
 	const auto shuf4_lo = Dup128VecFromValues(di8,
-		0, -1, 4, -1, 1, -1, 5, -1,
-		2, -1, 6, -1, 3, -1, 7, -1);
+		HWY_ENDIAN_LOHI(0, -1), HWY_ENDIAN_LOHI(4, -1),
+		HWY_ENDIAN_LOHI(1, -1), HWY_ENDIAN_LOHI(5, -1),
+		HWY_ENDIAN_LOHI(2, -1), HWY_ENDIAN_LOHI(6, -1),
+		HWY_ENDIAN_LOHI(3, -1), HWY_ENDIAN_LOHI(7, -1));
 	const auto shuf4_hi = Dup128VecFromValues(di8,
-		8, -1, 12, -1, 9, -1, 13, -1,
-		10, -1, 14, -1, 11, -1, 15, -1);
+		HWY_ENDIAN_LOHI(8, -1), HWY_ENDIAN_LOHI(12, -1),
+		HWY_ENDIAN_LOHI(9, -1), HWY_ENDIAN_LOHI(13, -1),
+		HWY_ENDIAN_LOHI(10, -1), HWY_ENDIAN_LOHI(14, -1),
+		HWY_ENDIAN_LOHI(11, -1), HWY_ENDIAN_LOHI(15, -1));
 
 	const auto shuf_lo = BitCast(di16, bands == 3 ? shuf3_lo : shuf4_lo);
 	const auto shuf_hi = BitCast(di16, bands == 3 ? shuf3_hi : shuf4_hi);
@@ -105,24 +119,32 @@ vips_reduceh_uchar_hwy(VipsPel *pout, VipsPel *pin,
 	 * r0 g0 b0 r1 g1 b1 r2 g2 b2 r3 g3 b3
 	 */
 	alignas(16) static constexpr int8_t tbl3_lo[16] = {
-		0, -1, 3, -1, 1, -1, 4, -1,
-		2, -1, 5, -1, -1, -1, -1, -1
+		HWY_ENDIAN_LOHI(0, -1), HWY_ENDIAN_LOHI(3, -1),
+		HWY_ENDIAN_LOHI(1, -1), HWY_ENDIAN_LOHI(4, -1),
+		HWY_ENDIAN_LOHI(2, -1), HWY_ENDIAN_LOHI(5, -1),
+		-1, -1, -1, -1
 	};
 	alignas(16) static constexpr int8_t tbl3_hi[16] = {
-		6, -1, 9, -1, 7, -1, 10, -1,
-		8, -1, 11, -1, -1, -1, -1, -1
+		HWY_ENDIAN_LOHI(6, -1), HWY_ENDIAN_LOHI(9, -1),
+		HWY_ENDIAN_LOHI(7, -1), HWY_ENDIAN_LOHI(10, -1),
+		HWY_ENDIAN_LOHI(8, -1), HWY_ENDIAN_LOHI(11, -1),
+		-1, -1 -1, -1
 	};
 
 	/*  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
 	 * r0 g0 b0 a0 r1 g1 b1 a1 r2 g2 b2 a2 r3 g3 b3 a3
 	 */
 	alignas(16) static constexpr int8_t tbl4_lo[16] = {
-		0, -1, 4, -1, 1, -1, 5, -1,
-		2, -1, 6, -1, 3, -1, 7, -1
+		HWY_ENDIAN_LOHI(0, -1), HWY_ENDIAN_LOHI(4, -1),
+		HWY_ENDIAN_LOHI(1, -1), HWY_ENDIAN_LOHI(5, -1),
+		HWY_ENDIAN_LOHI(2, -1), HWY_ENDIAN_LOHI(6, -1),
+		HWY_ENDIAN_LOHI(3, -1), HWY_ENDIAN_LOHI(7, -1)
 	};
 	alignas(16) static constexpr int8_t tbl4_hi[16] = {
-		8, -1, 12, -1, 9, -1, 13, -1,
-		10, -1, 14, -1, 11, -1, 15, -1
+		HWY_ENDIAN_LOHI(8, -1), HWY_ENDIAN_LOHI(12, -1),
+		HWY_ENDIAN_LOHI(9, -1), HWY_ENDIAN_LOHI(13, -1),
+		HWY_ENDIAN_LOHI(10, -1), HWY_ENDIAN_LOHI(14, -1),
+		HWY_ENDIAN_LOHI(11, -1), HWY_ENDIAN_LOHI(15, -1)
 	};
 
 	const auto shuf_lo = BitCast(di16,

libvips/convolution/convi_hwy.cpp
@@ -83,6 +83,12 @@ constexpr DI32 di32;
 #define HWY_LANES_CONSTEXPR
 #endif
 
+#if HWY_IS_BIG_ENDIAN
+#define HWY_ENDIAN_LOHI(lo, hi) hi, lo
+#else
+#define HWY_ENDIAN_LOHI(lo, hi) lo, hi
+#endif
+
 HWY_ATTR void
 vips_convi_uchar_hwy(VipsRegion *out_region, VipsRegion *ir, VipsRect *r,
 	int32_t ne, int32_t nnz, int32_t offset, const int32_t *HWY_RESTRICT offsets,
@@ -146,24 +152,28 @@ vips_convi_uchar_hwy(VipsRegion *out_region, VipsRegion *ir, VipsRect *r,
 				auto bottom = LoadU(du8, /* bottom line */
 					p + offsets[i + 1]);
 
-				auto source = InterleaveLower(top, bottom);
-				auto pix = BitCast(di16, InterleaveLower(source, zero));
+				auto source = InterleaveLower(HWY_ENDIAN_LOHI(top, bottom));
+				auto pix = BitCast(di16,
+					InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 				sum0 = ReorderWidenMulAccumulate(di32, pix, mmk, sum0,
 					/* byref */ sum1);
 
-				pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+				pix = BitCast(di16,
+					InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 				sum2 = ReorderWidenMulAccumulate(di32, pix, mmk, sum2,
 					/* byref */ sum3);
 
-				source = InterleaveUpper(du8, top, bottom);
-				pix = BitCast(di16, InterleaveLower(source, zero));
+				source = InterleaveUpper(du8, HWY_ENDIAN_LOHI(top, bottom));
+				pix = BitCast(di16,
+					InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 				sum4 = ReorderWidenMulAccumulate(di32, pix, mmk, sum4,
 					/* byref */ sum5);
 
-				pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+				pix = BitCast(di16,
+					InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 				sum6 = ReorderWidenMulAccumulate(di32, pix, mmk, sum6,
 					/* byref */ sum7);
@@ -175,24 +185,28 @@ vips_convi_uchar_hwy(VipsRegion *out_region, VipsRegion *ir, VipsRect *r,
 				 */
 				auto top = LoadU(du8, p + offsets[i]);
 
-				auto source = InterleaveLower(top, zero);
-				auto pix = BitCast(di16, InterleaveLower(source, zero));
+				auto source = InterleaveLower(HWY_ENDIAN_LOHI(top, zero));
+				auto pix = BitCast(di16,
+					InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 				sum0 = ReorderWidenMulAccumulate(di32, pix, mmk, sum0,
 					/* byref */ sum1);
 
-				pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+				pix = BitCast(di16,
+					InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 				sum2 = ReorderWidenMulAccumulate(di32, pix, mmk, sum2,
 					/* byref */ sum3);
 
-				source = InterleaveUpper(du8, top, zero);
-				pix = BitCast(di16, InterleaveLower(source, zero));
+				source = InterleaveUpper(du8, HWY_ENDIAN_LOHI(top, zero));
+				pix = BitCast(di16,
+					InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 				sum4 = ReorderWidenMulAccumulate(di32, pix, mmk, sum4,
 					/* byref */ sum5);
 
-				pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+				pix = BitCast(di16,
+					InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 				sum6 = ReorderWidenMulAccumulate(di32, pix, mmk, sum6,
 					/* byref */ sum7);

libvips/resample/reducev_hwy.cpp
@@ -83,6 +83,12 @@ constexpr DI32 di32;
 #define HWY_LANES_CONSTEXPR
 #endif
 
+#if HWY_IS_BIG_ENDIAN
+#define HWY_ENDIAN_LOHI(lo, hi) hi, lo
+#else
+#define HWY_ENDIAN_LOHI(lo, hi) lo, hi
+#endif
+
 HWY_ATTR void
 vips_reducev_uchar_hwy(VipsPel *pout, VipsPel *pin,
 	int32_t n, int32_t ne, int32_t lskip, const int16_t *HWY_RESTRICT k)
@@ -140,24 +146,28 @@ vips_reducev_uchar_hwy(VipsPel *pout, VipsPel *pin,
 			auto bottom = LoadU(du8, p); /* bottom line */
 			p += l1;
 
-			auto source = InterleaveLower(top, bottom);
-			auto pix = BitCast(di16, InterleaveLower(source, zero));
+			auto source = InterleaveLower(HWY_ENDIAN_LOHI(top, bottom));
+			auto pix = BitCast(di16,
+				InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 			sum0 = ReorderWidenMulAccumulate(di32, pix, mmk, sum0,
 				/* byref */ sum1);
 
-			pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+			pix = BitCast(di16,
+				InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 			sum2 = ReorderWidenMulAccumulate(di32, pix, mmk, sum2,
 				/* byref */ sum3);
 
-			source = InterleaveUpper(du8, top, bottom);
-			pix = BitCast(di16, InterleaveLower(source, zero));
+			source = InterleaveUpper(du8, HWY_ENDIAN_LOHI(top, bottom));
+			pix = BitCast(di16,
+				InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 			sum4 = ReorderWidenMulAccumulate(di32, pix, mmk, sum4,
 				/* byref */ sum5);
 
-			pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+			pix = BitCast(di16,
+				InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 			sum6 = ReorderWidenMulAccumulate(di32, pix, mmk, sum6,
 				/* byref */ sum7);
@@ -168,24 +178,28 @@ vips_reducev_uchar_hwy(VipsPel *pout, VipsPel *pin,
 			auto top = LoadU(du8, p);
 			p += l1;
 
-			auto source = InterleaveLower(top, zero);
-			auto pix = BitCast(di16, InterleaveLower(source, zero));
+			auto source = InterleaveLower(HWY_ENDIAN_LOHI(top, zero));
+			auto pix = BitCast(di16,
+				InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 			sum0 = ReorderWidenMulAccumulate(di32, pix, mmk, sum0,
 				/* byref */ sum1);
 
-			pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+			pix = BitCast(di16,
+				InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 			sum2 = ReorderWidenMulAccumulate(di32, pix, mmk, sum2,
 				/* byref */ sum3);
 
-			source = InterleaveUpper(du8, top, zero);
-			pix = BitCast(di16, InterleaveLower(source, zero));
+			source = InterleaveUpper(du8, HWY_ENDIAN_LOHI(top, zero));
+			pix = BitCast(di16,
+				InterleaveLower(HWY_ENDIAN_LOHI(source, zero)));
 
 			sum4 = ReorderWidenMulAccumulate(di32, pix, mmk, sum4,
 				/* byref */ sum5);
 
-			pix = BitCast(di16, InterleaveUpper(du8, source, zero));
+			pix = BitCast(di16,
+				InterleaveUpper(du8, HWY_ENDIAN_LOHI(source, zero)));
 
 			sum6 = ReorderWidenMulAccumulate(di32, pix, mmk, sum6,
 				/* byref */ sum7);

libvips/resample/shrinkv_hwy.cpp
@@ -79,6 +79,12 @@ constexpr int32_t max_bits = 1 << 8;
 #define HWY_LANES_CONSTEXPR
 #endif
 
+#if HWY_IS_BIG_ENDIAN
+#define HWY_ENDIAN_LOHI(lo, hi) hi, lo
+#else
+#define HWY_ENDIAN_LOHI(lo, hi) lo, hi
+#endif
+
 HWY_ATTR void
 vips_shrinkv_add_line_uchar_hwy(VipsPel *pin,
 	int32_t ne, uint32_t *HWY_RESTRICT sum)
@@ -105,8 +111,10 @@ vips_shrinkv_add_line_uchar_hwy(VipsPel *pin,
 		auto sum0 = LoadU(du32, &sum[x + 0 * N / 2]);
 		auto sum1 = LoadU(du32, &sum[x + 1 * N / 2]);
 
-		sum0 = Add(sum0, BitCast(du32, InterleaveLower(du16, pix0, zero)));
-		sum1 = Add(sum1, BitCast(du32, InterleaveUpper(du16, pix0, zero)));
+		sum0 = Add(sum0, BitCast(du32,
+			InterleaveLower(du16, HWY_ENDIAN_LOHI(pix0, zero))));
+		sum1 = Add(sum1, BitCast(du32,
+			InterleaveUpper(du16, HWY_ENDIAN_LOHI(pix0, zero))));
 
 		StoreU(sum0, du32, &sum[x + 0 * N / 2]);
 		StoreU(sum1, du32, &sum[x + 1 * N / 2]);

commit b3e3a529bae209c68e99abf92bae2c8cf29cbd51

reduceh: fix missing comma in fallback shuffle table (#5144)
Fixes a regression introduced in commit 46dde2e.

libvips/resample/reduceh_hwy.cpp
@@ -129,7 +129,7 @@ vips_reduceh_uchar_hwy(VipsPel *pout, VipsPel *pin,
 		HWY_ENDIAN_LOHI(6, -1), HWY_ENDIAN_LOHI(9, -1),
 		HWY_ENDIAN_LOHI(7, -1), HWY_ENDIAN_LOHI(10, -1),
 		HWY_ENDIAN_LOHI(8, -1), HWY_ENDIAN_LOHI(11, -1),
-		-1, -1 -1, -1
+		-1, -1, -1, -1
 	};
 
 	/*  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15